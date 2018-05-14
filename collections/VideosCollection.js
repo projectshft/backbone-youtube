1ef2ca964843b66753116f06365d4572d3cff401
Roy Kim <rykim79@gmail.com>
2018-05-14 08:06:18 -0400

    fixed indenting

--- collections/VideosCollection.js
+++ collections/VideosCollection.js
@@ -1,5 +1,5 @@
 var VideosCollection = Backbone.Collection.extend({
-url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=' + config.MY_KEY + '&',
+  url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=' + config.MY_KEY + '&',
   model: VideoModel,
 
   parse: function(response) {
