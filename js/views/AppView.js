029db1a94fb34d02dfccdcbaf2e2bc5e9dff5e75
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-08-17 12:14:27 -0400

    add appModel videoModel and videosCollection files, set up click event and test

--- js/views/AppView.js
+++ js/views/AppView.js
@@ -1,1 +1,16 @@
 //js/views/AppView
+
+var AppView = Backbone.View.extend({
+  el: $('body'),
+
+//event --> when the search button is clicked --> video list is created
+  events: {
+    'click .search-btn': 'createVideosList',
+    'click .search-btn': alert('search button was clicked'),
+  },
+
+  createVideosList : function(){
+    console.log( 'CreateVideoList function was invoked' );
+  }
+
+});
