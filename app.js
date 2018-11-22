0c4e30216f78c4b91f1012c7de3548a8499ef550
Edward Barton <edward.barton@icloud.com>
2018-11-21 21:34:30 -0500

    initialized app model with videos

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const ytAPI = `AIzaSyDHgux6pyvNhGIGufPIEF5E-qePUfMdKqo`;
+const ytAPI = `{API KEY}`;
 
 const app = new App();
-// Initialize app with a videos
-app.get('videos').fetchVideos(app.get('query'));
-app.set('current_video', app.get('videos').models[0]);
-
 const appView = new AppView({model: app});
--- models/App.js
+++ models/App.js
@@ -1,3 +1,7 @@
 const App = Backbone.Model.extend({
-  defaults: () => ({videos: new Videos(), current_video: null, query: 'puppies'})
+  defaults: () => ({videos: new Videos(), current_video: null, query: 'puppies'}),
+
+  initialize() {
+    this.get('videos').fetchVideos(this.get('query')); // Initialize app with videos
+  }
 });
