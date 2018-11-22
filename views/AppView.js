c1b153a4b2c7ab56bbf45b0bf840fd696abd9d8a
Edward Barton <edward.barton@icloud.com>
2018-11-21 21:36:19 -0500

    commented logic

--- views/AppView.js
+++ views/AppView.js
@@ -4,17 +4,19 @@ const AppView = Backbone.View.extend({
     'keypress .search-videos': 'addVideos',
     'click .video-list-item': 'changeCurrentVideo'
   },
+  
   initialize() {
     this.$input = $(`.search-videos`);
     this.$videoList = $(`.video-list`);
 
-    this.listenTo(this.model.get('videos'), 'reset', () => { // user searches
+    this.listenTo(this.model.get('videos'), 'reset', () => { // Collection resets once user searches
       this.renderVideo();
       this.renderVideoList();
     });
-    this.listenTo(this.model, 'change:current_video', this.renderVideo) // user selects side video list item
+    this.listenTo(this.model, 'change:current_video', this.renderVideo) // Curent video changes once user selects side video
   },
 
+  // Fetches videos from ytAPI and adds them to videos collection once a user searches and hits "Enter"
   addVideos(e) {
     if(e.keyCode === 13 && this.$input.val()) {
       this.model.set('query', this.$input.val());
@@ -23,13 +25,15 @@ const AppView = Backbone.View.extend({
     }
   },
   
+  // Renders view for current video
   renderVideo() {
     const videoView = new VideoView({
-      model: this.model.get('current_video') || this.model.get('videos').models[0]
+      model: this.model.get('current_video') || this.model.get('videos').models[0] // current_video null by default
     });
     this.$('.main').html(videoView.render().el);
   },
 
+  // Renders side video list views
   renderVideoList() {
     this.$videoList.empty();
     for(let i = 1; i < this.model.get('videos').models.length; i++) {
@@ -39,6 +43,7 @@ const AppView = Backbone.View.extend({
     }
   },
 
+  // Changes the current_video model to the associated model the user clicks on
   changeCurrentVideo(e) {
     let newVideoID = e.target.dataset.id;
     let newVideo = this.model.get('videos').findWhere({id: newVideoID});
