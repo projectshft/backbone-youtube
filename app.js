968967f1a522c8b82210d219a7de697551b21e36
Edward Barton <edward.barton@icloud.com>
2018-11-21 19:08:41 -0500

    Initialized app with videos

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const ytAPI = `{API KEY}`;
+const ytAPI = `AIzaSyDHgux6pyvNhGIGufPIEF5E-qePUfMdKqo`;
+
 const app = new App();
+// Initialize app with a videos
+app.get('videos').fetchVideos(app.get('query'));
+app.set('current_video', app.get('videos').models[0]);
+
 const appView = new AppView({model: app});
