ffe2c2c21cc2b09561fd423b64e436616c955a14
Rhys Lutsky <rhyslutsky@gmail.com>
2018-11-24 09:51:24 -0500

    add YouTube sample responses to script.js

--- script.js
+++ script.js
@@ -1,19 +1,189 @@
 var appModel = new AppModel();
 
-appModel.get('videoList').add([{
-        videoId: "T3XA8Jyf9ng",
-        title: "Cobra Weave",
-        image: "https://youtu.be/T3XA8Jyf9ng",
-        description: "Cody weaves"
+appModel.get('videoList').add([
+    /*** API response*/
+{
+  "kind": "youtube#searchListResponse",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/AtSYxAjK9Ydk5BUQQNfOHH0Nsxo\"",
+  "nextPageToken": "CAUQAA",
+  "regionCode": "US",
+  "pageInfo": {
+    "totalResults": 1000000,
+    "resultsPerPage": 5
+  },
+  "items": [
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/zHdNqSx3AmLKuvNAGxXOMbnVMlk\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "wxBtwCZtDAg"
+      },
+      "snippet": {
+        "publishedAt": "2018-11-21T01:00:11.000Z",
+        "channelId": "UCZFhj_r-MjoPCFVUo3E1ZRg",
+        "title": "13-Year-Old FEARLESS Surfing Prodigy",
+        "description": "13-Year-Old Kai Williams was born to surf. Nobody works harder at their craft! COMMENT with your favorite beach sport! MORE No Days Off → Young Jamaican ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/wxBtwCZtDAg/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/wxBtwCZtDAg/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/wxBtwCZtDAg/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Whistle",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/E8GZG_CZfJeaVF75eZYmJHnGe0c\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "rj7xMBxd5iY"
+      },
+      "snippet": {
+        "publishedAt": "2017-11-12T11:09:52.000Z",
+        "channelId": "UCiiFGfvlKvX3uzMovO3unaw",
+        "title": "BIG WAVE SURFING COMPILATION 2017",
+        "description": "BIG WAVE SURFING COMPILATION 2017 ** REVISED **AMAZING FOOTAGE ** WITH 60-100FT- HUGE SURF Please Subscribe if You Would like to see More ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/rj7xMBxd5iY/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/rj7xMBxd5iY/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/rj7xMBxd5iY/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Absolutely Flawless",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/aFLvMIiSGn0894mONBhgYIdX3cg\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "nkhpGC10OVw"
+      },
+      "snippet": {
+        "publishedAt": "2017-04-09T17:18:44.000Z",
+        "channelId": "UCHeaHzQFLElUw__yG3SSzMg",
+        "title": "World's best surfing 2017",
+        "description": "World's best surfing 2017 — Enjoy the video. Rate, Comment, Share... Thanx Subscribe for new compilations: http://goo.gl/X017T If your Video is in this ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/nkhpGC10OVw/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/nkhpGC10OVw/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/nkhpGC10OVw/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Monthly Winners",
+        "liveBroadcastContent": "none"
+      }
+    },
+    {
+      "kind": "youtube#searchResult",
+      "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/f4B92xUeptcNLBwHd4cnYEjNyZE\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "eSwisMEtkBg"
+      },
+      "snippet": {
+        "publishedAt": "2018-09-02T01:56:19.000Z",
+        "channelId": "UCfn_qdZ1XMLRKIfMhexjooA",
+        "title": "What Surfing Is Actually Like",
+        "description": "The Gorpo I Use: https://amzn.to/2COZ1hQ The Mouth Mount I Use: https://amzn.to/2ClyqYM My Free Vlog Like A Pro Course: http://startavlog.com Favorite ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/eSwisMEtkBg/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/eSwisMEtkBg/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/eSwisMEtkBg/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Atua Mo'e",
+        "liveBroadcastContent": "none"
+      }
     },
 
     {
-        videoId:"T3XA8Jyf9ng",
-        title: "BIG WAVE SURFING COMPILATION 2017",
-        image: "https://i.ytimg.com/vi/rj7xMBxd5iY/default.jpg",
-        description: "BIG WAVE SURFING COMPILATION 2017 ** REVISED **AMAZING FOOTAGE ** WITH 60-100FT- HUGE SURF"
-    },
+      "kind": "youtube#searchResult",
+      "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/-xK93qgJ5SOMwKNHtBIEWCBQbZE\"",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "hc0lYWSJ0-0"
+      },
+      "snippet": {
+        "publishedAt": "2018-11-24T05:48:39.000Z",
+        "channelId": "UCuZSTHZf3vd7eVehhnotcsg",
+        "title": "Small Wave Surfing Secrets",
+        "description": "This is the next video in our Small Wave Surfing Series! We cover all the hints and tips you will need to make the most of small wave surfing! If you haven't seen ...",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/hc0lYWSJ0-0/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/hc0lYWSJ0-0/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/hc0lYWSJ0-0/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "How to Rip",
+        "liveBroadcastContent": "none"
+      }
+    }
+  ]
+} 
 ]);
+    
+
+
 
 var appView = new AppView({
     model: appModel
