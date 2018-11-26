e81155fa0f438b35bba73552be4d99f9535a0fe9
Betty <bmay.wanjiru@gmail.com>
2018-11-26 12:38:21 -0500

    include a gitignore file

--- Collections/upNextVideoCollection.js
+++ Collections/upNextVideoCollection.js
@@ -1,20 +1,24 @@
-//your 5 videos 
+//your 5 videos
 var upNextVideoCollection = Backbone.Collection.extend({
   model: currentlyPlayingVideo,
 
-//fetch your videos from the API
+  //fetch your videos from the API
   fetchVideos: function(query) {
     this.url = 'https://www.googleapis.com/youtube/v3/search?&key=AIzaSyDUXvHcn6NsWBtbRD_NWHReDjTPW0S8fz8&part=snippet&chart=mostpopular&type=video&q=' + query;
+    this.fetch({
+      reset: true
+    });
   },
 
-  parse: function (response) {
-    return response.items.map(function (vid){
+  parse: function(response) {
+    return response.items.map(function(vid) {
       return {
         videoTitle: vid.snippet.title,
         videoDescription: vid.snippet.description,
-        videoId: vid.id.videoID,
+        videoId: vid.id.videoId,
         thumbnail: vid.snippet.thumbnails.default.url
       }
     })
+    return result;
   }
 });
--- Models/AppModel.js
+++ Models/AppModel.js
@@ -3,8 +3,8 @@ var AppModel = Backbone.Model.extend({
   defaults: function () {
     return {
       videos: new upNextVideoCollection(),
-      currentVideo: null,//video currently playing in the webapp
+      currentVideo: null, //video currently playing in the webapp
       query: 'puppies'//default search criteria
     }
   }
-})
+});
