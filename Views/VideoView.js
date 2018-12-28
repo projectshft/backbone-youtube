c2f9f686ebee531a420140289ea19eaa166255c8
Rhys Lutsky <rhyslutsky@gmail.com>
2018-12-28 01:21:19 -0500

    update VideoView to represent each video

--- Views/VideoView.js
+++ Views/VideoView.js
@@ -1,12 +1,27 @@
-var VideoView = Backbone.View.extend({
-    className: 'thumbnails',
+//View for displaying EACH video item
 
+var VideoView = Backbone.View.extend({
+    tagName: 'li',
+    className: 'videoItem',
     template: Handlebars.compile($ ('#list-template').html()),
+
+    // initialize: function() {
+    //     this.listenTo(this.model, '')
+    // },
 
     render: function(){
         this.$el.html(this.template(this.model.toJSON()));
-
+        this.$el.html(html);
         return this;
-    }
+    },
+
+    events: {
+        'click .view-Video': 'onView'
+    },
+
+    // onView: function(){
+    //     //change position from list to currently playing
+    //     this.model
+    // }
 
 });
