0215fca0f29f24f002b028fccb14f4c4a6574e33
Rhys Lutsky <rhyslutsky@gmail.com>
2018-12-28 01:27:37 -0500

    create VideoListView

---
+++ Views/VideoListView.js
@@ -0,0 +1,25 @@
+//View to render list of videos from collection
+
+var VideoListView = Backbone.View.extend({
+    el: '#video-app',
+
+    //render view as soon as collection finishes sync with API
+    initialize: function(){
+        this.listenTo(this.collection, 'sync', this.render);
+    },
+
+    
+    render: function(){
+        //empty list container
+        var $list = this.$('ul.videos-list').empty();
+
+        //make new view for each model in collection
+        this.collection.each(function(model) {
+            var item = new VideoView({model: model});
+            $list.append(item.render().$el);
+        }, this);
+        return this;
+    },
+
+    
+})
