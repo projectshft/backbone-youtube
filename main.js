193f6d1fc39de19b62257cb2565b0830308ed04e
mcstreilein <madelinestreilein@Madelines-MacBook-Air.local>
2019-03-15 10:51:31 -0400

    creating search bar with autofocus in html/css

---
+++ main.js
@@ -0,0 +1,13 @@
+/* EVAL 4 
+
+Youtube API key: AIzaSyBg4MPAVQKAhUcWR6hpfHkV2CMq-t0Pges
+
+USER STORY 1: A user should be able to search something in the search bar, and get a list of videos back, 
+with one showing up in the main screen.
+
+USER STORY 2: When a user clicks a video in the list on the right, the main video should change to that video.
+
+USER STORY 3: Each time a user clicks a different video, the title and description beneath the main video should update as well.
+
+USER STORY 4: When the user first loads the page, there should be a default search so the page is not blank.
+*/
