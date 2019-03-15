7f9460778c7f2ec21fa4299aea22476ed254a5c4
Jason Doss <doss2k@gmail.com>
2019-03-15 14:11:30 -0400

    Add functionality for creating current video html and rendering it

---
+++ Views/CurrentVideoView.js
@@ -0,0 +1,12 @@
+// Creates the view for the current video via a Handlebars template and then renders that HTML
+
+const CurrentVideoView = Backbone.View.extend({
+  className: 'current-video',
+
+  template: Handlebars.compile($('#current-video-template').html()),
+
+  render() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+});
