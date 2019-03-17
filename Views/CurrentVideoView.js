af24c42b9dc410ad42d87d121c7947dae70b795c
Jason Doss <doss2k@gmail.com>
2019-03-17 16:37:55 -0400

    Small spacing fix

--- Views/CurrentVideoView.js
+++ Views/CurrentVideoView.js
@@ -5,7 +5,7 @@ const CurrentVideoView = Backbone.View.extend({
 
   template: Handlebars.compile($('#current-video-template').html()),
 
-  render: function() {
+  render: function () {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
   }
