df8b0b8a622ce1d5618b2c2b9f9900faf7cfc5dc
alice <alicectung@gmail.com>
2019-03-17 15:50:24 -0400

    Complete render video results list.

--- views/AppView.js
+++ views/AppView.js
@@ -1,10 +1,6 @@
 var AppView = Backbone.View.extend({
     el: $('body'),
 
-    //tagName: 'aside',  
-
-    template: Handlebars.compile($('#video-template').html()),
-
     events: {
         // AppView recognizes search clicked, calls function to grab the query
       'click .submit-search': 'fetchVideo',
@@ -15,10 +11,7 @@ var AppView = Backbone.View.extend({
 
       this.$searchInput = this.$('#search-input');
 
-      this.listenTo(this.model.get('videos'), 'reset', this.renderVideos);
-
-      this.$videoList1 = this.$('.card-1');
-      this.$videoList2 = this.$('.card-2');
+      this.listenTo(this.model.get('videos'), 'reset', this.render);
      
     },
 
@@ -33,40 +26,24 @@ var AppView = Backbone.View.extend({
           this.model.get('videos').fetchVideos(query);
     },
 
-
-
-    renderVideos: function() {
+    render: function() {
    
-      this.model.get('videos').each(function (v) {
-        //console.log('each video: ', v);
-        
-        this.renderVideo(v);
-      }, this);
+      this.model.get('videos').each(function (v) { // For each of the 5 unique models
+     
+
+        const title = v.get('title'); // Extract data info from video models
+        const description = v.get('description');
+        const thumbnails = v.get('thumbnails');
+  
+        var source = $('#video-template').html(); // Render to page with handlebars template
+        var template = Handlebars.compile(source)
+        var newHTML = template({title, description, thumbnails});
+        this.$('.card-1').append(newHTML);
+
+        return this;
+
+      });
     },
-
-    renderVideo: function(video) {
-      //console.log('RENDER each video!', video);
-
-      // takes array of 5 items
-      // compiles title & description into handlebars template
-      // append 5 models onto <aside> tagName/ class=card
-
-    
-      //console.log(this.model.attributes.videos.models[0].attributes);
-      const path = this.model.attributes.videos.models[0].attributes;
-      let json = JSON.stringify(path);
-      console.log(json);
-      this.$videoList1.html(this.template(json));
-
-      
-      //this.$videoList.append(beerView.render().el);
-
-      //this.$el.html(this.template(this.model.toJSON()));
-
-      //this.$videoList.html(this.template(this.model.toJSON()));
-
-      return this;
-    }
 
    
   });
