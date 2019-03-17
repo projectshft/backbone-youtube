05725705e2df3c72caf2147fb2d5ad74b6b240b5
Luc Saunders <lucsaunders@gmail.com>
2019-03-16 23:03:18 -0400

    Add templates for embedded video player and list of videos.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+/**************************************************
+  Google Dev project name: Backbone-Video-
+  API-Key: AIzaSyBhEipxN2X8twvxFy7Uck1ehRxzzyjZQXY
+ * ***********************************************/
+
 let appModel = new AppModel();
 
 let appView = new AppView({ model: appModel });
