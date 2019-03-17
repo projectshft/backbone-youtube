37c00e9b67f44c2c2411b9b6970c9d95dd020c67
Ritchie Fesperman <ritchie.fes@gmail.com>
2019-03-16 20:38:18 -0400

    add current video/description templates and render methods

--- collections/VideosCollection.js
+++ collections/VideosCollection.js
@@ -1,7 +1,7 @@
 //extend the Backbone Collection methods to VideosCollection
 var VideosCollection = Backbone.Collection.extend({
   // set VideosCollection model to VideoModel
-  model: VideoModel,
+  // model: VideoModel,
 
   fetchData: function(query) {
     this.url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q=${query}&type=video&videoDefinition=high&key=AIzaSyDcydR_2UMVTi4t1f58KHM-HrZ0HyS8MxE`
@@ -9,12 +9,12 @@ var VideosCollection = Backbone.Collection.extend({
   },
   //define the parse function to retrive the data that we want from what is returned from the server
   parse: function(response) {
-    console.log(response.items[0].snippet.title)
-    return response.items.map(index => ({
-      videoID: index.id.videoId,
-      title: index.snippet.title,
-      description: index.snippet.description,
-      thumbnail: index.snippet.thumbnails.medium.url
-    }));
+    return response.items.map(function(index){
+      return {
+        videoID: index.id.videoId,
+        title: index.snippet.title,
+        description: index.snippet.description
+      }
+    });
   }
 });
--- views/AppView.js
+++ views/AppView.js
@@ -3,7 +3,9 @@ var AppView = Backbone.View.extend({
   //define my el
   el: $('body'),
   //compile template for current video section
-  template: Handlebars.compile($('#recommended-video-template').html()),
+  templateR: Handlebars.compile($('#recommended-video-template').html()),
+  templateCV: Handlebars.compile($('#current-video-template').html()),
+  templateCVD: Handlebars.compile($('#current-video-description-template').html()),
   //create my event listeners:
   events: {
     'click .btn-search': 'notifyAppModelOfQueryChange',
@@ -13,7 +15,10 @@ var AppView = Backbone.View.extend({
   initialize: function() {
     //store the #search-input element as a JQuery object so we can have access to the JQuery methods later
     this.$searchInput = this.$('#search-input');
-    this.$recommendedListCol = this.$('.recommended-list-col')
+    this.$recommendedListCol = this.$('.recommended-list-col');
+    this.$currentVideoCol = this.$('.current-video-col');
+    this.$currentVideoDescriptionCol = this.$('.current-video-description-col');
+    console.log($('.current-video-description-col').html());
     //listen to changes in this model's videos key, if there is a change invoke a render function
     this.listenTo(this.model.get('videos'), 'reset', this.renderPage);
   },
@@ -33,14 +38,16 @@ var AppView = Backbone.View.extend({
   },
   //define renderPage, in response to initialize's change listener
   renderPage: function() {
-    // console.log(this.model.attributes.videos);
-    // this.$('recommended-list-col').html(this.template(this.model.toJSON()));
     this.$recommendedListCol.empty();
+    this.$currentVideoCol.empty();
+    this.$currentVideoDescriptionCol.empty();
     var recommendedArray = this.model.attributes.videos.models.splice(1,5);
-      recommendedArray.forEach(function(index){
-        this.$recommendedListCol.append(this.template(index.toJSON()));
-      }, this)
-    // return this
+    var topVideoIndex = this.model.attributes.videos.models[0];
+    recommendedArray.forEach(function(index){
+      this.$recommendedListCol.append(this.templateR(index.toJSON()));
+    }, this)
+    this.$currentVideoCol.html(this.templateCV(topVideoIndex.toJSON()));
+    this.$currentVideoDescriptionCol.html(this.templateCVD(topVideoIndex.toJSON()));
   }
 });
 
