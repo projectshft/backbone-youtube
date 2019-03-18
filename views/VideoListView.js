20ccb88a705d44aca475c7cd785d432665741d64
Jason Park <jason.j.park79@gmail.com>
2019-03-18 01:53:06 -0400

    some clean up. still sorry i disappointed you sean & edward

--- views/VideoListView.js
+++ views/VideoListView.js
@@ -2,6 +2,14 @@ var VideoListView = Backbone.View.extend({
 
   template: Handlebars.compile($('#playlist-template').html()),
 
+  events: {
+    
+  },
+
+  initialize: function() {
+    //some listento stuff
+  }
+
 
 
 });
