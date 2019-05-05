bdcd38239b58bb1e10fac53e0ab4984e329f6f63
Carson Poe <cgp6012@gmail.com>
2019-05-05 14:56:36 -0400

    updated appmodel

--- models/AppModel.js
+++ models/AppModel.js
@@ -1,10 +1,19 @@
-var AppModel = Backbone.Model.extend({
-    defaults: function(){
-        return {
-            query: '',
-          
-          //need to have the video got to the Collection
-            videoList: new VideoCollection
-        }
+const AppModel = Backbone.Model.extend({
+  defaults: function () {
+    return {
+      //appmodel needs the videocollection
+      videos: new VideosCollection(),
+      // playing video with the videoId
+      playing_video: null
     }
+  },
+  initialize: function () {
+    this.get("videos").fetch();
+  },
+
+  //function for playing video based on the videoId
+    setPlayingVideo: function (id) {
+    const playingVideo = this.get("videos").findWhere({ videoId: id });
+    this.set("playing_video", playingVideo);
+  },
 });
