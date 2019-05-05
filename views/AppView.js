e0a5327aea6fbbcb245631bafb3aa1142b3a2a77
Carson Poe <cgp6012@gmail.com>
2019-05-05 14:49:47 -0400

    appview

--- views/AppView.js
+++ views/AppView.js
@@ -1,30 +1,67 @@
-var appView = Backbone.view.extend({
-  el:$('body'),
+const AppView = Backbone.View.extend({
 
+  el: $("body"),
+// need inputs for the search
   events: {
-    'keypress #searchBar': 'searchVideos'
-
+    "click #search-button": "getSearchQueryOnClick",
+    "keypress #search-input": "getSearchQueryOnEnter",
+    "click .thumbnail-view": "changePlayingVideo"
+  },
+// event listeners
+  initialize: function () {
+    this.listenTo(this.model, "change:playing_video", this.renderPlayingVideo);
+    this.listenTo(this.model.get("videos"), "add", this.renderThumbnail);
+    this.listenTo(this.model.get("videos"), "update", this.setInitialPlayingVideo);
   },
 
-  initialize: function () {
-   //initial a get request
-   this.model.get('videoList').getVideos(this.model.get('query'));
- },
+  getSearchQueryOnClick: function () {
+    const searchQuery = $("#search-input").val();
+    if (searchQuery) {
+      this.model.get("videos").fetchVideoData(searchQuery);
+      this.$(".thumbnails-container").empty();
+    }
+    this.resetInput();
+  },
 
- searchVideos: function(e) {
-    if (e.which === 13) {
-      //set a variable to the value of the user's search
-      var query = $('#searchBar').val();
-        if (!query) {
-          //error handling to account for an empty search query
-          alert('Enter valid search information');
-        } else {
-        //set the query attribute to the search information
-        this.model.set('query', query)
-        this.model.get('videoList').getVideos(this.model.get('query'));
+  getSearchQueryOnEnter: function (e) {
+    if (e.key === "Enter") {
+      const searchQuery = $("#search-input").val();
+      if (searchQuery) {
+        this.model.get("videos").fetchVideoData(searchQuery);
+        this.$(".thumbnails-container").empty();
       }
-      $('#searchBar').val('')
+      this.resetInput();
     }
   },
-
+  resetInput: function () {
+    $("#search-input").val("").focus();
+  },
+  // need render for thumbnail
+  renderThumbnail: function (video) {
+    const thumbnailView = new ThumbnailView({ model: video });
+    this.$(".thumbnails-container").append(thumbnailView.render().el);
+  },
+  // need to set first on list to the first playing video
+  setInitialPlayingVideo: function () {
+    if (this.model.get("videos").at(0)) {
+      $(".playing-video").show();
+      $(".error").hide();
+      const initialPlayingVideo = this.model.get("videos").at(0).get("videoId");
+      this.model.setPlayingVideo(initialPlayingVideo);
+    } else {
+      $(".playing-video").hide();
+      $(".error").show();
+    }
+  },
+  // need function to change the video playing by getting a new videoId
+  changePlayingVideo: function (event) {
+    const playingVideoId = $(event.currentTarget).data().id;
+    this.model.setPlayingVideo(playingVideoId);
+  },
+  // need the playing video when a change event happens
+  renderPlayingVideo: function () {
+    const template = Handlebars.compile($("#playing-video-template").html());
+    this.$(".playing-video")
+      .html(template(this.model.get("playing_video").toJSON()));
+  }
 });
