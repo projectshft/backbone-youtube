17070f12dbb85db998987837ff1015485a7c80f7
dposse <dposse.125@gmail.com>
2019-06-29 14:13:20 -0400

    Added three main sections in html: search-bar, main-video, sidebar-videos

--- sensitiveInfo.js
+++ sensitiveInfo.js
@@ -2,9 +2,7 @@
 //don't need to have a getter for api key - it's already built into api url
 
 const SensitiveInfo = () => {
-
   const API_KEY = 'AIzaSyA5ua2d9-GqR3Gb0yrrJn7mQFJSjlW0UAA';
-
   const YT_API_URL_BASE = `https://www.googleapis.com/youtube/v3/search?maxResults=10&part=snippet&fields=items(id(videoId),snippet(title,description,thumbnails(default(url))))&type=video&key=${API_KEY}&q=`;
 
   const getAPIBaseUrl = () => YT_API_URL_BASE;
@@ -12,5 +10,4 @@ const SensitiveInfo = () => {
   return {
     getAPIBaseUrl
   }
-
 };
