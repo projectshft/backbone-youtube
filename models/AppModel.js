72b0e2348c88a1bf9f42128ce7a2c729d977be88
K. Bender <kristinajbender@gmail.com>
2019-06-30 01:15:31 -0400

    Create column scrolling and add main video title/description div

--- models/AppModel.js
+++ models/AppModel.js
@@ -5,17 +5,20 @@ var AppModel = Backbone.Model.extend({
 
         current_video: null,
 
-        current_URL: ''
+        current_URL: '', 
+
+        key: ''
       };
     },
 
     initialize : function () {
-        this.set('current_URL', 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=cats&type=video&key=AIzaSyCPPmlPfkv3U89LcDYIMstVbhT0ZN7MNPg');
+        this.set('key', 'AIzaSyD5dy-5GDx7outd6JdIGjjRFJxmkKKBmrw'); 
+        this.set('current_URL', 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=cats&type=video&key=' + this.get('key'));
         this.get('videos').url = this.get('current_URL'); 
     },
 
     setSearchUrl : function (query) {
-        var newUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=' + query + '&type=video&key=AIzaSyCPPmlPfkv3U89LcDYIMstVbhT0ZN7MNPg';
+        var newUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=' + query + '&type=video&key=' + this.get('key');
         this.set('current_URL', newUrl);
         this.get('videos').url = this.get('current_URL'); 
     },
