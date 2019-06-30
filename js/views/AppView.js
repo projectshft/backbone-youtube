1c6f54d6a65dc1ad7cc27b3fe25db18a5f56b431
dposse <dposse.125@gmail.com>
2019-06-29 20:15:38 -0400

    Created side video template

--- js/views/AppView.js
+++ js/views/AppView.js
@@ -15,19 +15,28 @@ var AppView = Backbone.View.extend({
     this.listenTo(this.model, 'change:main_video', this.renderPage);
   },
   renderPage: function() {
-    this.renderMainVideo();
+    //get main video model first since both render functions need to know what model to use/skip
+    var mainVideoModel = this.model.get('main_video');
+    debugger;
+    this.renderMainVideo(mainVideoModel);
     //render rest of videos in sidebar, excluding main video
+    this.renderVideos(mainVideoModel);
   },
-  renderMainVideo: function() {
+  renderMainVideo: function(video) {
     console.log('Rendering main video');
     this.$mainVideo.empty();
     //make a new MainView, pass in main video as model
-    var mainView = new MainVideoView({ model: this.model.get('main_video') });
+    var mainView = new MainVideoView({ model: video });
     //append the views render
     this.$mainVideo.append(mainView.render().el);
   },
-  renderVideos: function() {
-
+  renderVideos: function(videoToOmit) {
+    this.model.get('videos').each( function(video) {
+      
+        console.log('hey');
+        if (video === videoToOmit)
+          console.log('whoah buddy');
+    }, this);
   },
   newSearch: function() {
     this.model.searchForVideos(this.$searchInput.val());
