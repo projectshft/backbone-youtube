a678c85dcc33972fa4bf075e63616c5522b87bee
unknown <nathaniel.poli@gmail.com>
2019-07-01 12:56:10 -0400

    added a default search so the fields are not blank

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ var ESC_KEY = 27;
 var mykey = config.API_KEY;
 
 var appView = new AppView({model: appModel});
+//Setting an initial search so the page is not blank
+appView.model.get('videos').setURL('pokemon')
+appView.model.get('videos').fetch({reset:true})
