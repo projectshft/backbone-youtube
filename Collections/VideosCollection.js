8c08dd15c6232386446b82be7da634d845c3a92e
Kyle Kimber <kimber.kyle@gmail.com>
2019-08-31 19:10:54 -0400

    Created the video collection with the required date form the api response

--- Collections/VideosCollection.js
+++ Collections/VideosCollection.js
@@ -1,20 +1,17 @@
+const VideoCollection = Backbone.Collection.extend({
 
-//Create the videos collection and setup the url. I am using a collection because I need to return data as an array. I'm going to have to use fetch and possibly parse. 
-var VideosCollection = Backbone.Collection.extend({
-  url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&part=player&q=',
-//Connect VideosCollection to VideoModel
   model: VideoModel,
-  
-//??? What does this mean? I need to use parse to work with the YT API
-fetchVideos: function (query) {
-  this.url = this.url + query + 's&type=video&key=AIzaSyDkzTwB15n7nv3ezxBh7wjbw36VwmUmdO8'
-  console.log(this.url)
-  this.fetch({reset: true})
 
-//   this.create({
-//     id: id,
-//     thumbnail: thumbnail,
-//     title: tilte,
-//     description: description
-//   }, { wait: true });
- },
+  //only taking in the relavent data from the api
+  parse = (response) => {
+
+      return response.items.map((videoList) => {
+          return {
+              title: videoList.snippet.title,
+              video_id: videoList.id.videoId,
+              description: videoList.snippet.description,
+              img_url: videoList.snippet.thumbnails.default.url
+          };
+      });
+  }
+}); 
