d90838b5513cdea5e037a527be35260c59207fee
Billy DeLashmutt <delashmuttwa@vt.edu>
2019-10-11 23:48:35 -0400

    Add AppView constructor

--- utils.js
+++ utils.js
@@ -1,1 +1,25 @@
-// ALL BACKBONE CONSTRUCTORS HERE
+/////////////////
+// COLLECTIONS //
+/////////////////
+
+/////////////////
+//    MODELS   //
+/////////////////
+
+/////////////////
+//    VIEWS    //
+/////////////////
+
+// APPVIEW //
+var AppView = Backbone.View.extend({
+  el: $('body'),
+
+  events: {
+    'click #search-button': 'alertUser'
+  },
+
+  alertUser: function () {
+    alert("You clicked search!")
+  }
+
+})
