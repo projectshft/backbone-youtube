cc9c5f1a41fc652b86b096a4da6e143d1c9673df
Billy DeLashmutt <delashmuttwa@vt.edu>
2019-10-12 21:57:51 -0400

    Add CurrentVideoView, VideoModel, and VideosCollection

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-var appView = new AppView();
+const appModel = new AppModel();
+
+appModel.get('videos').addVideo("KXGLSF3MuZw", "Vaughn Gittin Jr. - Drift King of The Ring[Extended Cut]")
+
+const appView = new AppView({
+  model: appModel
+});
+
+// VALUES TO PULL FROM JSON RESPONSE
+// 
--- utils.js
+++ utils.js
@@ -1,25 +1,91 @@
 /////////////////
+//    MODELS   //
+/////////////////
+
+// APPMODEL //
+const AppModel = Backbone.Model.extend({
+  defaults: function () {
+    return {
+      videos: new VideosCollection()
+    }
+  }
+})
+
+const VideoModel = Backbone.Model.extend({
+  defaults: function () {
+    return {
+      id: '',
+      title: '',
+      description: ''
+    }
+  }
+})
+
+
+/////////////////
 // COLLECTIONS //
 /////////////////
 
-/////////////////
-//    MODELS   //
-/////////////////
+// VIDEOSCOLLECTION //
+const VideosCollection = Backbone.Collection.extend({
+  model: VideoModel,
+
+  addVideo: function (id, title, description) {
+    this.add({
+      id: id,
+      title: title,
+      description: description
+    })
+  }
+
+})
+
+
 
 /////////////////
 //    VIEWS    //
 /////////////////
 
 // APPVIEW //
-var AppView = Backbone.View.extend({
+const AppView = Backbone.View.extend({
   el: $('body'),
 
-  events: {
-    'click #search-button': 'alertUser'
+  initialize: function () {
+    // this.listenTo(this.model.get('videos'), 'add', this.renderCurrentVideo);
+    this.renderVideos();
   },
 
-  alertUser: function () {
-    alert("You clicked search!")
+  // events: {
+  //   'click #search-button': 'searchVideo'
+  // },
+
+  // searchVideo: function () {
+  //   this.model.get('videos').addVideo(
+  //   );
+  // },
+
+  renderCurrentVideo: function (video) {
+    const currentVideoView = new CurrentVideoView({
+      model: video
+    });
+    this.$('#current-video').append(currentVideoView.render().el)
+  },
+
+  renderVideos: function () {
+    this.model.get('videos').each(function (m) {
+      this.renderCurrentVideo(m);
+    }, this);
   }
 
+})
+
+// CURRENTVIDEOVIEW //
+const CurrentVideoView = Backbone.View.extend({
+  template: Handlebars.compile($('#current-video-template').html()),
+
+  render: function () {
+    this.$el.html(this.template(this.model.toJSON()));
+
+    return this;
+  }
 })
