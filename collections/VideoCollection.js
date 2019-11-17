d6ef873815f71bdee4052cd2f6b0ec8724d1d8ee
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-11-17 16:35:39 -0500

    updated api and collection

--- collections/VideoCollection.js
+++ collections/VideoCollection.js
@@ -1,16 +1,11 @@
 var VideosCollection = Backbone.Collection.extend({
-  url: 'null',
+  url: ' https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyA7g39h3EESAB4anzZSQxG9z0MsLM-U7c0',
   model: VideoModel,
 
 
   parse: function (response) {
-    return response.map.items} => {
+    return response.map.items}
       
-      return {
-        title: item.title,
-      };
-debugger;
-      
-
+    
 
 });
