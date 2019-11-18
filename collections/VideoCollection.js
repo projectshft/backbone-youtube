d8a8574b2403399db934957ea3a42d49d95e15ed
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-11-18 11:36:15 -0500

    updated collection with search and play functions

--- collections/VideoCollection.js
+++ collections/VideoCollection.js
@@ -1,11 +1,31 @@
 var VideosCollection = Backbone.Collection.extend({
-  url: ' https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyA7g39h3EESAB4anzZSQxG9z0MsLM-U7c0',
+  url: '  https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyA7g39h3EESAB4anzZSQxG9z0MsLM-U7c0&q=cool videos',
   model: VideoModel,
 
 
   parse: function (response) {
-    return response.map.items}
-      
-    
+    return response.items.map(function (data){
 
-});
+      videoObj = {
+        id: data.id.videoId,
+        title: data.snippet.title,
+        description: data.snippet.description,
+        thumbnailUrl: data.snippet.thumbnails.default.url,
+
+      };
+      return videoObj
+    })
+      
+  },
+
+  searchVideo: function (query) {
+    this.reset()
+    this.url= 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyA7g39h3EESAB4anzZSQxG9z0MsLM-U7c0&q=${query}&type=videos'
+    this.fetch()
+  },
+
+  playNewVideo: function (onClickThumbnail) {
+    this.reset()
+    this.add(this.findwhere({id: onClickThumbnail}))
+  }
+})
