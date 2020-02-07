a50d432e7b2df060b9eca006d9f10008d99bbbc2
BENZABOSS <joyceben33@gmail.com>
2020-02-07 16:39:41 -0500

    Reorganizing

---
+++ views/AppView.js
@@ -0,0 +1,28 @@
+var AppView = Backbone.View.extend({
+
+    el: $('.container'),
+    
+    event: {
+
+    },
+
+    intitialize: function() {
+        this.$searchInput = this.$('#video-search');
+        this.$searchButton = this.$('.search-btn')
+        this.$videoPlayer = this.$('.video-player');
+        this.$videoQue = this.$('.video-que');
+ 
+    },
+
+    firstFetch: function() {
+        this.model.get('videos').fetch({
+            data: {
+                q: 'airplanes',
+                part: 'snippet',
+                key: config.MY_KEY
+            }
+        })
+    }
+
+
+})
