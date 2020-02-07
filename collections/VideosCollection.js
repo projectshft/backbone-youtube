1f227a04aa74e42327cb7ac50a3988bcfd5a200a
BENZABOSS <joyceben33@gmail.com>
2020-02-07 13:48:54 -0500

    Added scripts in the index.HTML file

--- collections/VideosCollection.js
+++ collections/VideosCollection.js
@@ -1,8 +1,11 @@
 var VideosColection = Backbone.Collection.extend({
 
+    url: 'https://www.googleapis.com/youtube/v3/search',
 
+    model: VideoModel,
 
+    parse: function(response) {
+        // need to return the array of data for that query
+    }
 
-
-
-})
+});
