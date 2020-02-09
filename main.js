ecdc43ad06334b31e28b704973a0f1d76e394d82
Jay Averitt <59446910+jatiger@users.noreply.github.com>
2020-02-09 13:16:23 -0500

    Finalized root files for Youtube Backbone Frontend

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ var onErrorHandler = function (collection, response, options) {
     console.log(response.responseText);
     alert('Woah there!  You have an API problem, matey. \nPlease check the API fetch function parameters.');
   };
-
+  
 // Main Backbone framework
 
   var appModel = new AppModel();
-
+  
   var appView = new AppView({
     model: appModel
   });
