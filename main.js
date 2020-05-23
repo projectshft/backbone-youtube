eec38207d331a2a28bf611fe008d746eb51b1992
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-05-23 12:23:46 -0400

    added AppView

--- main.js
+++ main.js
@@ -6,3 +6,28 @@ var AppModel = Backbone.Model.extend({
     }
   }
 })
+
+var AppView = Backbone.View.extend({
+  el: $('body'),
+
+  initialize: function () {
+    this.listenTo(this.model.get('videos'), 'reset', this.renderSidebarVideos);
+  },
+
+  renderSidebarVideo: function(vid) {
+    console.log(vid);
+    var sidebarVideoView = new SidebarVideoView({ model: vid});
+    $('.sidebar').append(sidebarVideoView.render().el);
+    console.log('rendered ' + sidebarVideoView);
+  },
+
+  renderSidebarVideos: function() {
+    console.log('rendering sidebar videos');
+    console.log(this.model.get('videos'));
+    this.model.get('videos').each(function (vid) {
+      this.renderSidebarVideo(vid);
+      console.log('yeet');
+    }, this);
+    console.log('yeeters');
+  }
+})
