a6548ff8c2e8a04b52f673273b0fd293dd40434e
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-05-23 12:42:26 -0400

    added SidebarVideoView

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var AppModel = Backbone.Model.extend({
   defaults: function() {
     return {
+      //current_video represents video being shown on main display
       current_video: null,
       videos: new VideosCollection()
     }
@@ -11,9 +12,11 @@ var AppView = Backbone.View.extend({
   el: $('body'),
 
   initialize: function () {
+    //renders sidebar videos on a reset event
     this.listenTo(this.model.get('videos'), 'reset', this.renderSidebarVideos);
   },
 
+  //creates a SidebarVideoView from a video model, appending it to the sidebar
   renderSidebarVideo: function(vid) {
     console.log(vid);
     var sidebarVideoView = new SidebarVideoView({ model: vid});
@@ -21,13 +24,23 @@ var AppView = Backbone.View.extend({
     console.log('rendered ' + sidebarVideoView);
   },
 
+  //loops through collection of videos stored in AppModel- videos, rendering each video
   renderSidebarVideos: function() {
     console.log('rendering sidebar videos');
     console.log(this.model.get('videos'));
     this.model.get('videos').each(function (vid) {
       this.renderSidebarVideo(vid);
-      console.log('yeet');
     }, this);
-    console.log('yeeters');
+  }
+})
+
+//view for each sidebar video
+var SidebarVideoView = Backbone.View.extend({
+  className: "sidebar-card",
+  template: Handlebars.compile($('#sidebar-video-template').html()),
+
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
   }
 })
