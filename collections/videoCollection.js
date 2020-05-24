0554b417b3d8fcea494af0dec46046f3c3af9b9f
MCola <mcolapietro502@gmail.com>
2020-05-24 15:11:12 -0400

    added video title and description beneath main video; added styling to div containers

--- collections/videoCollection.js
+++ collections/videoCollection.js
@@ -1,10 +1,14 @@
 var VideoCollection = Backbone.Collection.extend({
   model: VideoModel,
 
-  addVideo: function (videoId) {
+  
+  addVideo: function (videoId, videoThumbnail, title, description) {
     this.add({
       videoId: videoId,
+      videoThumbnail: videoThumbnail,
+      title: title,
+      description: description
+
     });
   },
-
 })
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var appModel = new AppModel();
 
-appModel.get('videos').addVideo('mLyOj_QD4a4');
+appModel.get('videos').addVideo('mLyOj_QD4a4','https://i.ytimg.com/vi/mLyOj_QD4a4/default.jpg', 'Leeroy Jenkins HD 1080p', 'Leeroy Jenkins in HD 1080p ENJOY!');
 appModel.get('videoList').addVideoList('mLyOj_QD4a4','https://i.ytimg.com/vi/mLyOj_QD4a4/default.jpg', 'Leeroy Jenkins HD 1080p', 'Leeroy Jenkins in HD 1080p ENJOY!');
 appModel.get('videoList').addVideoList('mLyOj_QD4a4','https://i.ytimg.com/vi/mLyOj_QD4a4/default.jpg', 'Leeroy Jenkins HD 1080p', 'Leeroy Jenkins in HD 1080p ENJOY!');
 appModel.get('videoList').addVideoList('mLyOj_QD4a4','https://i.ytimg.com/vi/mLyOj_QD4a4/default.jpg', 'Leeroy Jenkins HD 1080p', 'Leeroy Jenkins in HD 1080p ENJOY!');
