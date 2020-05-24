ee94ad640a92295affcf3db05f6d7c18177b34d6
Pierre Lourens <plourens3@gmail.com>
2020-05-23 21:46:00 -0400

    adds basic handlebars templating for mainvideo

--- views/AppView.js
+++ views/AppView.js
@@ -7,10 +7,28 @@ var AppView = Backbone.View.extend({
 
   initialize: function () {
     this.$searchInput = this.$("#search-input");
+    this.$mainVideoBox = this.$("#main-content");
+    this.renderMainVideo();
   },
 
   searchYouTube: function () {
     console.log(this.$searchInput.val());
     // send the value from the search bar to the VideosCollection
+  },
+
+  // render main video
+  renderMainVideo: function () {
+    // handlebars stuff
+
+    // saving for later use, but don't want to get ahead of myself
+    // var mainVideoModel = this.model.get("videos").at(0);
+
+    // we need a model to pass to handlebars
+    var mainVideoModel = {
+      videoId: "shlsRbZaB60",
+    };
+
+    var mainView = new MainVideoView({ model: mainVideoModel });
+    this.$mainVideoBox.append(mainView.render().el);
   },
 });
--- views/MainVideoView.js
+++ views/MainVideoView.js
@@ -2,4 +2,12 @@ var MainVideoView = Backbone.View.extend({
   events: {},
 
   initialize: function () {},
+
+  template: Handlebars.compile($("#main-video-template").html()),
+
+  render: function () {
+    this.$el.html(this.template(this.model));
+
+    return this;
+  },
 });
