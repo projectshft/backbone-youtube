09b9034c15896ff628ab603351a4602895a4fe60
Pierre Lourens <plourens3@gmail.com>
2020-05-23 23:08:12 -0400

    Cleans up unnecessary console logging in AppModel

--- models/AppModel.js
+++ models/AppModel.js
@@ -7,17 +7,12 @@ var AppModel = Backbone.Model.extend({
 
   updateVideosCollection: function (searchTerm) {
     // we want a default search if none is passed by AppView
-    console.log("searchTerm is ", searchTerm);
     if (typeof searchTerm === "undefined") {
       searchTerm = "dogs";
     }
 
-    console.log("searchTerm is ", searchTerm);
-    
     // want to grab the collection of videos to update it
     var allVideos = this.get("videos");
-
-    console.log("The VideosCollection before the call is ", allVideos);
 
     // updates the url for the VideosCollection, based on the user's search term
     allVideos.url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${searchTerm}&key=AIzaSyDQfKcKKGwULrhZccV7sPY1aKn_GlZ3dHM`;
