8eaf7b14b9e45e483903429bf4306528cf0712a6
Pierre Lourens <plourens3@gmail.com>
2020-05-25 12:46:42 -0400

    Adding comment to explain future use of AllSuggestedVideoView

--- views/AllSuggestedVideosView.js
+++ views/AllSuggestedVideosView.js
@@ -1,1 +1,4 @@
+// the reason this view exists is for an optional extension
+// (if I work on it after turning in project). This view would be used to create
+// infinite scrolling
 var AllSuggestedVideosView = Backbone.View.extend({});
