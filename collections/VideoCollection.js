2ee9df146c9dacb11034efa1c0b16ebb55b8e65a
Clyde Weed <clydeweed@gmail.com>
2020-05-26 03:43:38 -0400

    Added AppModel and moved url from VideoModel to VideoCollection.

--- collections/VideoCollection.js
+++ collections/VideoCollection.js
@@ -1,6 +1,6 @@
 //This will be the collection of models (videos) from the server. It needs to be displayed in the right pane.
 
 
-// var VideoCollection = Backbone.Collection.extend({
-//   url: 'https://www.googleapis.com/youtube/v3/videos?chart=mostPopular&maxResults=5&regionCode=US&key=AIzaSyDFf_6NTONo5rmGPHmJGMnpLqkXzS8tm7g'
-// });
+var VideoCollection = Backbone.Collection.extend({
+  urlRoot: 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=' + {{search-term}} + '&key=AIzaSyDFf_6NTONo5rmGPHmJGMnpLqkXzS8tm7g',
+});
