bedcee96bd2e2c5c969721b571efcee747866a8c
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-05-25 23:14:30 -0400

    refactored functions for loading main video, fixed formatting, style

--- main.js
+++ main.js
@@ -6,72 +6,77 @@ var AppModel = Backbone.Model.extend({
       videos: new VideosCollection()
     }
   },
+
+  //updates VideoCollection url and sends API request with searchTerm (input from searchbar)
   fetchNewVideos: function(searchTerm) {
     var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=' + searchTerm + '&type=video&key=AIzaSyChaaV8WZehiaxwY3hxoyEe5yuLTQX2O9M';
+    //setting VideosCollection url to include searchTerm
     this.get('videos').url = url;
-    this.get('videos').fetch({ reset: true });
+    this.get('videos').fetch({
+      reset: true
+    });
   }
-})
+});
 
 var AppView = Backbone.View.extend({
   el: $('body'),
 
-  initialize: function () {
-    //renders sidebar videos on a reset event
-    this.listenTo(this.model.get('videos'), 'reset', this.setMainVideo);
-    this.listenTo(this.model.get('videos'), 'reset', this.renderSidebarVideos);
-    this.listenTo(this.model.get('videos'), 'reset', this.renderMainVideo);
+  initialize: function() {
     this.current_video_view = null;
+    this.listenTo(this.model.get('videos'), 'reset', this.renderMainVideo);
   },
 
+  //events to search for videos when button is clicked, and load a new main video from the sidebar
   events: {
-    'click .card': 'loadNewMainVideo',
+    'click .card': 'renderMainVideo',
     'click .btn': 'search'
   },
 
   search: function() {
+    //fetches searchTerm from searchbar input
     var searchTerm = $('.form-control').val();
     this.model.fetchNewVideos(searchTerm)
   },
 
-  loadNewMainVideo: function(e) {
-    var modelID = $(e.currentTarget).data().id;
-    //this.current_video_view.remove();
-    this.model.current_video_model = this.model.get('videos').remove(modelID);
-    this.current_video_view = new MainVideoView({ model : this.model.current_video_model});
-    $('.main-video').empty();
-    this.renderSidebarVideos();
-    this.renderMainVideo();
-  },
-
   //creates a SidebarVideoView from a video model, appending it to the sidebar
-  renderSidebarVideo: function(vid) {
-    var sidebarVideoView = new SidebarVideoView({ model: vid});
+  renderSidebarVideo: function(videoModel) {
+    var sidebarVideoView = new SidebarVideoView({
+      model: videoModel
+    });
     $('.sidebar').append(sidebarVideoView.render().el);
-    console.log('rendered ' + sidebarVideoView);
   },
 
-  setMainVideo: function(videoId) {
-    var mainVideo = this.model.get('videos').shift(0);
-    this.model.current_video_model = mainVideo;
-    this.current_video_view = new MainVideoView({model: mainVideo});
-  },
-
-  renderMainVideo: function() {
+  //renders main video from click event, when the page is initialized, or when a new search is made
+  renderMainVideo: function(e) {
+    //if statement to load first video as main video when page is loaded
+    // otherwise, loads clicked video from sidebar
+    if (!this.model.current_video_model) {
+      this.model.current_video_model = this.model.get('videos').shift(0);
+    } else {
+      // modelID is the ID for each model (as well as the videoID for Youtube)
+      var modelID = $(e.currentTarget).data().id;
+      //clicked sidebar video is removed form sidebar, and moved to main video
+      this.model.current_video_model = this.model.get('videos').remove(modelID);
+    }
+    //empties main-video-container before loading new main video
     $('.main-video-container').empty();
-    var mainVideoView = new MainVideoView({ model: this.model.current_video_model});
-    $('.main-video-container').append(mainVideoView.render().el);
+    this.current_video_view = new MainVideoView({
+      model: this.model.current_video_model
+    });
+    $('.main-video-container').append(this.current_video_view.render().el);
+    //renders sidebar videos after to prevent main video from showing in the sidebar
+    this.renderSidebarVideos();
   },
 
   //loops through collection of videos stored in AppModel- videos, rendering each video
   renderSidebarVideos: function() {
+    //empties sidebar before re-rendering
     $('.sidebar').empty();
-    this.model.get('videos').each(function (vid) {
+    this.model.get('videos').each(function(vid) {
       this.renderSidebarVideo(vid);
     }, this);
   }
-})
-
+});
 //view for each sidebar video
 var SidebarVideoView = Backbone.View.extend({
   className: "sidebar-card",
@@ -81,8 +86,8 @@ var SidebarVideoView = Backbone.View.extend({
     this.$el.html(this.template(this.model.toJSON()));
     return this;
   }
-})
-
+});
+//view for main video,
 var MainVideoView = Backbone.View.extend({
   className: "main-video",
   template: Handlebars.compile($('#main-video-template').html()),
@@ -97,7 +102,8 @@ var VideoModel = Backbone.Model.extend({
   defaults: {
     title: '',
     description: '',
-    thumbnail_url: ''
+    thumbnail_url: '',
+    id: ''
   }
 })
 
@@ -112,6 +118,7 @@ var VideosCollection = Backbone.Collection.extend({
         title: item.snippet.title,
         description: item.snippet.description,
         thumbnail_url: item.snippet.thumbnails.high.url,
+        //id is used to grab video models from collection and also create the Iframe for a youtube video
         id: item.id.videoId
       }
     }, this);
@@ -124,4 +131,6 @@ var appView = new AppView({
   model: appModel
 });
 
-appModel.get('videos').fetch({ reset: true });
+appModel.get('videos').fetch({
+  reset: true
+});
