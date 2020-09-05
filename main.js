d22c7acc8891d3870fdd32f81e024baf1156bf37
PJ Foley <pjfoley@gmail.com>
2020-09-05 15:04:18 -0700

    initial commit. html. templates

---
+++ main.js
@@ -0,0 +1,4 @@
+// google youtube API key AIzaSyBcGykn4L8KYnJzrg6o-adli3S3kHVwEtU
+// classes: .main-stage .sidebar-vids
+// templates:  main-stage-template  sidebar-vids-template  main-desc-template
+// handlebar madlibs: {{videoId}} {{videoTitle}}{{videoDesc}}{{thumbnail}}
