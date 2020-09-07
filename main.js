e7ec540e618fce9cd5d93977a96c45d282b97196
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-09-07 15:21:22 -0400

    changes the URL formatting

--- main.js
+++ main.js
@@ -11,10 +11,16 @@ var VideoModel = Backbone.Model.extend({
 //create a VideosCollection var
 var VideosCollection = Backbone.Collection.extend({
     model: VideoModel,
-    //query: $('input').val(),
-    //connect the Youtube API to ur collection
-    //  url: `https://www.googleapis.com/youtube/v3/search?key=AIzaSyBCFVX7-Ic64kujaRXZD5boR3tDaaS9-C4&type=video&part=snippet&maxResults=5&q=${query}`,
+    //connect the Youtube API to our collection
+    // default : {
+    //     searchParameter: this.$('input').val()
+    // },
 
+    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBCFVX7-Ic64kujaRXZD5boR3tDaaS9-C4&type=video&part=snippet&maxResults=5&q=auburntigers',        
+
+    // initialize: function (){
+    //     this.url();
+    // }
     //data to parse for the 5 videos
     //data.items[i].snippet.title
     //data.items[i].snippet.description
@@ -27,9 +33,10 @@ var VideosCollection = Backbone.Collection.extend({
 var AppModel = Backbone.Model.extend({
     defaults: function () {
         return {
-            videos: new VideosCollection()
+                videos: new VideosCollection()       
         }
     }
+    
 });
 
 // create a videos view for the side videos display
@@ -57,14 +64,15 @@ var AppView = Backbone.View.extend({
     // create a function that will search for YT videos when enter is pressed
     createVideo: function (e) {
         if (e.keyCode === 13) {
-            let query = $('input').val();
-            console.log(query);
+            this.$('input').val();
+            
         }        
     },
 
     initialize: function() {
+        this.listenTo(this.model.get('videos'), 'reset', this.renderVideos);
         this.renderVideos();
-    },
+    }, 
     //create a render video function that creates a new videos view and appends the data to the DOM
     renderVideo: function (video) {
         var videosView = new VideosView({ model: video });
@@ -82,14 +90,8 @@ var AppView = Backbone.View.extend({
 // initializes a view and model
 var appModel = new AppModel;
 //hardcode in some side videos to understand the flow of the HTML
-appModel.get('videos').add([
-  {title: 'Auburns Final Play in Iron Bowl', thumbnail: 'https://i.ytimg.com/vi/9ZmZ-ZrRYD0/default.jpg'},
-  {title: 'Boe Jackson, a legacy', thumbnail: 'https://i.ytimg.com/vi/0TkD_C7fB-Q/default.jpg'},
-  {title: 'War Damn Eagle', thumbnail: 'https://i.ytimg.com/vi/VO9n74Jzilg/default.jpg'},
-  {title: 'Bo Nicks, next Cam Newton?', thumbnail: 'https://i.ytimg.com/vi/KBETP_VvEOs/default.jpg'},
-  {title: 'Cam Newton highlights', thumbnail: 'https://i.ytimg.com/vi/XocQcgQzrOk/default.jpg'}
-]);
+appModel.get('videos').fetch({ reset: true });
 
 var appView = new AppView({ model: appModel });
 
-
+console.log(appModel.get('videos').url);
