6d65b1b0c9cc8af7881ac6e98bf40490dd2018b5
Daniel Renteria <d.renteria_a@yahoo.com>
2020-09-07 19:54:16 -0400

    Add hardcode videos

--- views/SideVideoView.js
+++ views/SideVideoView.js
@@ -2,7 +2,7 @@ var SideVideoView = Backbone.View.extend({
     className: 'side-video',
 
     //Write template function here
-    
+    template: Handlebars.compile($('#side-video-template').html()),
 
     render: function() {
         //template this.model.toJSON()
