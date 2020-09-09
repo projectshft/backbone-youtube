944444455046a291c6a6dbb40c314fd70df497db
PJ Foley <pjfoley@gmail.com>
2020-09-09 16:35:59 -0700

    cleaning out unneeded code

--- main.js
+++ main.js
@@ -84,208 +84,180 @@ var VideoCollection = Backbone.Collection.extend({
 // renderApp: fill .main-stage and trigger sidebarView
 
 var AppView = Backbone.View.extend({
-  el: $('body'),
+      el: $('body'),
 
-  events: {
-    'click .submit': 'performSearch',
-    'keyup #video-search' : 'checkSubmit'
+      events: {
+        'click .submit': 'performSearch',
+        'keyup #video-search': 'checkSubmit'
 
-  },
+      },
 
-  initialize: function () {
+      initialize: function () {
 
-    // this.listenTo(this.model.get('videos'), 'add', this.renderVideoEntry);
-    this.$sidebarVids = this.$('.sidebar-vids');
-    this.$videoSearch = this.$('#video-search');
-    this.$mainStage = this.$('.main-stage');
-    this.$mainDesc = this.$('.main-desc');
-    // start app display default search
-    this.performSearch();
-    this.renderVideoList();
-    // when new search inits, re-render video list and main stage
-    this.listenTo(this.model.get('videos'), 'reset', this.renderVideoList);
-    this.listenTo(this.model.get('videos'), 'reset', this.renderMainStage);
-    //TODO when mainStage is working
-    // this.listenTo(this.model.get('videos'), 'reset', this.renderDetailsView);
-  },
-  // models created from collection fetch. now create views
+        // this.listenTo(this.model.get('videos'), 'add', this.renderVideoEntry);
+        this.$sidebarVids = this.$('.sidebar-vids');
+        this.$videoSearch = this.$('#video-search');
+        this.$mainStage = this.$('.main-stage');
+        this.$mainDesc = this.$('.main-desc');
 
-  renderVideoList: function () {
-    console.log('renderVideoList()');
-    $('.sidebar-vids').empty();
-    this.model.get('videos').each(function (vid) {
-      this.renderVideoEntry(vid);
-    }, this);
-  },
 
-  // create and render views of video listings
-  renderVideoEntry: function (video) {
-    console.log('renderVideoEntry()');
-    // console.log('this videoEntry ', this);
-    var videoView = new VideoView({
-      model: video
-    });
-    this.$sidebarVids.append(videoView.render().el);
-  },
+        // start app display default search
+        this.$videoSearch.val(searchTerms);
+        this.performSearch();
+        this.renderVideoList();
+        // when new search inits, re-render video list and main stage
+        this.listenTo(this.model.get('videos'), 'reset', this.renderVideoList);
+        this.listenTo(this.model.get('videos'), 'reset', this.renderMainStage);
+        //TODO when mainStage is working
+        // this.listenTo(this.model.get('videos'), 'reset', this.renderDetailsView);
+      },
+      // models created from collection fetch. now create views
 
-  renderMainStage: function (video) {
-    var stageView = new StageView({
-      model: video
-    });
-    console.log('renderMainStage() in appView');
-    $('.main-stage').empty();
-    console.log('this is now ', this);
-    var main = this.model.get('videos').findWhere({
-      on_stage: true
-    });
-    console.log('main is ', main);
-    // TODO seems like the perfect plan, but won't work. Needs binding?!?
-    // main.$mainStage.append(stageView.render().el);
-  },
+      renderVideoList: function () {
+        console.log('renderVideoList()');
+        this.$sidebarVids.empty();
+        this.model.get('videos').each(function (vid) {
+          this.renderVideoEntry(vid);
+        }, this);
+      },
 
-  renderDetailsView: function (video) {
-    //TODO or combo with renderMainStage
-    /*     console.log('renderDetailsView in appView');
-        var detailsView = new DetailsView({ model: video});
-        this.$mainDesc.append(detailsView.render().el); */
-  },
+      // create and render views of video listings
+      renderVideoEntry: function (video) {
+        console.log('renderVideoEntry()');
+        // console.log('this videoEntry ', this);
+        var videoView = new VideoView({
+          model: video
+        });
+        this.$sidebarVids.append(videoView.render().el);
+      },
 
-  checkSubmit: function(e){
-    console.log($('#video-search').val());
-    if(e.keyCode == 13){
-      this.performSearch()
-    }
-  },
+      renderMainStage: function () {
+        // var stageView = new StageView({  // wrong plavec
+        //   model: video
+        // });
+        console.log('renderMainStage() in appView');
+        this.$mainStage.empty();
+        this.model.get('videos').each(function (vid) {
+              if (vid.get('on_stage')) {
+                this.renderMainStage(vid);
+              }}, this);
+          },
 
-  performSearch: function () {
-    console.log('performSearch() with ', this.$videoSearch.val());
-    console.log('versus global ', searchTerms);
-    // send search terms to collection (via global) and fetch
-    // this.model.get('videos').doSearch( // trying for dataflow but no-go
-    // clean up edge cases (don't waste searches) OR send default search
-    // for first run
-    if (this.$videoSearch === '' && searchTerms === '') {
-      return;
-    } else if (searchTerms === '') {
-      searchTerms = this.$videoSearch.val();
-      appModel.get('videos').fetch({
-        reset: true
+
+          checkSubmit: function (e) {
+            console.log($('#video-search').val());
+            if (e.keyCode == 13) {
+              this.performSearch()
+            }
+          },
+
+          performSearch: function () {
+            console.log('performSearch() with ', this.$videoSearch.val());
+            console.log('versus global ', searchTerms);
+            // send search terms to collection (via global) and fetch
+            // this.model.get('videos').doSearch( // trying for dataflow but no-go
+            // clean up edge cases (don't waste searches) OR send default search
+            // for first run
+            if (this.$videoSearch === '' && searchTerms === '') {
+              return;
+            } else if (searchTerms === '') {
+              searchTerms = this.$videoSearch.val();
+              appModel.get('videos').fetch({
+                reset: true
+              });
+            } else { // default for first run
+              appModel.get('videos').fetch({
+                reset: true
+              });
+            }
+          },
+
+          renderOnStage: function () {
+            console.log('renderOnStage()');
+            // when VideoModel changes to "on_stage true" refresh
+          }
       });
-    } else { // default for first run
-      appModel.get('videos').fetch({
-        reset: true
-      });
-    }
-  },
 
-  renderOnStage: function () {
-    console.log('renderOnStage()');
-    // when VideoModel changes to "on_stage true" refresh
-  }
-});
+    // VideoView create
+    // className: video-listing
+    // template: sidebar-vids-template
+    // TODO events click - not working
 
-// VideoView create
-// className: video-listing
-// template: sidebar-vids-template
-// TODO events click - not working
+    var VideoView = Backbone.View.extend({
+      className: 'video-listing',
+      template: Handlebars.compile($('#sidebar-vids-template').html()),
+      events: {
+        'click .video-listing': 'setOnStage'
+      },
 
-var VideoView = Backbone.View.extend({
-  className: 'video-listing',
-  template: Handlebars.compile($('#sidebar-vids-template').html()),
-  events: {
-    'click .video-listing': 'setOnStage'
-  },
+      initalize: function () {
+        console.log('videoView inits');
+        this.listenTo(this.model, 'on_stage', this.switchMain); // needs to toggle class visibilty (.d-none)
+      },
 
-  initalize: function () {
-    console.log('videoView inits');
-    this.listenTo(this.model, 'on_stage', this.switchMain); // needs to toggle class visibilty (.d-none)
-  },
+      setOnStage: function () {
+        console.log('selected a vid for OnStage');
+        this.model.set('on_stage, true');
+        //TODO set other models on_stage to false.
+      },
 
-  setOnStage: function () {
-    console.log('selected a vid for OnStage');
-    this.model.set('on_stage, true');
-    //TODO set other models on_stage to false.
-  },
+      switchMain: function () {
+        //TODO
+      },
 
-  switchMain: function () {
-    //TODO
-  },
-
-  render: function () {
-    // hide the current video because it's on the main stage
-    if (this.model.get('on_stage')) {
-      console.log('detected on_stage');
-      this.$el.html(this.template(this.model.toJSON())).addClass('d-none');
-    }
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;
-  }
-
-});
-
-// StageView create
-// className: stage
-// template: #main-stage-template
-// initialize: listen to videoModel change: onstage; refresh render
-
-var StageView = Backbone.View.extend({
-  className: 'stage',
-
-  template: Handlebars.compile($('#main-stage-template').html()),
-
-  /* initialize: function () {
-    this.listenTo(this.model.get('videos'), 'change:on_stage', this.changeStageVideo);
-    this.listenTo(this.model.get('videos'), 'reset', this.changeStageVideo);
-  },
-
-  changeStageVideo: function () {
-    console.log('changing stage video');
-    //replace with findwhere
-    this.model.get('videos').each(function (vid) { // find first onStage true
-      if (vid.model.get('on_stage')) {
-        console.log('found featured video');
+      render: function () {
+        // hide the current video because it's on the main stage
+        if (this.model.get('on_stage')) {
+          console.log('detected on_stage');
+          this.$el.html(this.template(this.model.toJSON())).addClass('d-none');
+        }
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
       }
-      }, this);
-  }, */
 
-  render: function () {
-    console.log('render main stage');
-    console.log('mainView render this is ', this);
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;
-  }
+    });
 
-});
+    // StageView create
+    // className: stage
+    // template: #main-stage-template
+    // initialize: listen to videoModel change: onstage; refresh render
 
-// DetailsView create
-// className: details
-// template: #main-details-template
-// initialize: listen to videoModel change: onstage, trigger render
+    var StageView = Backbone.View.extend({
+      className: 'stage',
 
-var DetailsView = Backbone.View.extend({
-  className: 'details',
+      template: Handlebars.compile($('#main-stage-template').html()),
 
-  template: Handlebars.compile($('#main-details-template').html()),
-  // should pattern same as main view - render findwhere result from appView
-  render: function () {
-    console.log('render details view');
-    if (this.model.get('on_stage')) {
-      console.log('detected on_stage');
-      this.$el.html(this.template(this.model.toJSON()));
-    } else {
-      return null;
-    }
-    return this;
-  }
-});
+      /* initialize: function () {
+        this.listenTo(this.model.get('videos'), 'change:on_stage', this.changeStageVideo);
+        this.listenTo(this.model.get('videos'), 'reset', this.changeStageVideo);
+      },
+
+      changeStageVideo: function () {
+        console.log('changing stage video');
+        //replace with findwhere
+        this.model.get('videos').each(function (vid) { // find first onStage true
+          if (vid.model.get('on_stage')) {
+            console.log('found featured video');
+          }
+          }, this);
+      }, */
+
+      render: function () {
+        console.log('render main stage');
+        console.log('mainView render this is ', this);
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+      }
+
+    });
 
 
-// kickoff logic
-// new AppModel()
-// new AppView from AppModel
-// populate default search and API_KEY
+    // kickoff logic
+    // new AppModel()
+    // new AppView from AppModel
+    // populate default search and API_KEY
 
-var appModel = new AppModel();
-var appView = new AppView({
-  model: appModel
-});
+    var appModel = new AppModel();
+    var appView = new AppView({
+      model: appModel
+    });
