306822e4f9a490de5a4036c721a36586c6e580bc
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-10-17 12:55:41 -0400

    adds in search functionality

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var VideosCollection = Backbone.Collection.extend({
     model: VideoModel,
     //connect the Youtube API to our collection
    
-    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBCFVX7-Ic64kujaRXZD5boR3tDaaS9-C4&type=video&part=snippet&maxResults=5&q=auburntigers',
+    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBCFVX7-Ic64kujaRXZD5boR3tDaaS9-C4&type=video&part=snippet&maxResults=5&q=auburntigers`,
     // create parse function to only return the attributes we want for our video model
     parse: function (response) {
         return response.items.map(function (vid) {
@@ -28,6 +28,11 @@ var VideosCollection = Backbone.Collection.extend({
             }
         }, this);
     },
+
+    changeSearch: function(searchParameter) {
+        this.url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBCFVX7-Ic64kujaRXZD5boR3tDaaS9-C4&type=video&part=snippet&maxResults=5&q=${searchParameter}`
+        this.fetch({ reset: true });
+    }
 });
 
 // create an AppModel var, includes the main video
@@ -35,11 +40,15 @@ var AppModel = Backbone.Model.extend({
     defaults: function () {
         return {
                 videos: new VideosCollection(),
-                
-                searchParameter: null,
+                //holds the search input
+                searchParameter: null
         }
-    }
+    },
     
+    changeSearchParameter: function(searchParameter){
+        this.set('searchParameter', searchParameter);
+        this.get('videos').changeSearch(searchParameter)
+    }
 });
 
 // create a main vid
@@ -77,16 +86,16 @@ var AppView = Backbone.View.extend({
     },
     // create a function that will search for YT videos when enter is pressed
     createVideo: function (e) {
-        if (e.keyCode === 13) {
-            this.$('input').val();
-            
+        var searchParameter = this.$('input').val();
+        if (e.keyCode === 13 && searchParameter != '') {
+            this.model.changeSearchParameter(searchParameter);      
         }        
     },
 
     initialize: function() {
         this.listenTo(this.model.get('videos'), 'reset', this.renderVideos);
         // create a listener to render new videos when the search parameter is changed
-        this.listenTo(this.model, 'change:searchParamater', this.renderVideos);
+        this.listenTo(this.model, 'change:searchParameter', this.renderVideos);
         this.renderVideos();
         // this.renderMain();
     }, 
