c97304ea7426e01c03a195b9064f5b217d04080f
unknown <joshkamrass@gmail.com>
2021-04-08 15:17:15 -0400

    adds ability to load hard-coded data for testing

--- models/AppModel.js
+++ models/AppModel.js
@@ -12,6 +12,20 @@ var AppModel = Backbone.Model.extend({
 
   setCurrentVideo: function (newVideos) {
     this.set("currentVideo", newVideos.models[0]);
+  },
+
+  //Loads data to test rather than fetching data from server
+  loadData: function(data) {
+    //Run data through the video collections parser
+    data = this.get("videos").parse(data);
+    //Run each array through the video model parser
+    var newVideoModel = new VideoModel();
+    debugger;
+    data = data.map(function (obj) {
+      return newVideoModel.parse(obj);
+    })
+    debugger;
+    this.get("videos").add(data);
   }
   
 });
