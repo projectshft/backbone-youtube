f1a2a56ab0bbfb08e0bea6da8374f63970e08535
Daniel Piston <danielpiston@Daniels-MacBook-Pro.local>
2021-05-15 19:00:33 -0400

    handlebars error instead of api error, so major progress

--- main.js
+++ main.js
@@ -1,35 +1,82 @@
-// Videos collection - add API fetch here 
-var VideosCollection = Backbone.Collection.extend({
-  url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=cercle&type=video&videoEmbeddable=true&key=AIzaSyCjo4u-wcr_ExFNPxiYlWZP3LLr-ythijE",
-
-  model: VideoModel,
-
-  parse: function (response) {
-    // need to make this an array that is mapped
-    // getting error cannot read property prototype of undefined for whole collection
-
-    return response.map(function (i) {
-      return {
-        videoId: response.items[i].id.videoId,
-        videoThumbnail: response.items[i].snippet.thumbnails.default.url,
-        videoTitle: response.items[i].snippet.title,
-        videoDesc: response.items[i].snippet.description,
-      }
-    });
-  }
-});
-
 var VideoModel = Backbone.Model.extend({
   defaults: function () {
     return {
-      videoId: '',
-      videoThumbnamil: '',
-      videoTitle: '',
-      videoDesc: ''
-    }
+      videoId: "",
+      videoThumbnamil: "",
+      videoTitle: "",
+      videoDesc: "",
+    };
+  },
+
+  url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=cercle&type=video&videoEmbeddable=true&key=AIzaSyCjo4u-wcr_ExFNPxiYlWZP3LLr-ythijE",
+
+  parse: function (response) {
+    return {
+        videoId: response.items[0].id.videoId,
+        videoThumbnail: response.items[0].snippet.thumbnails.default.url,
+        videoTitle: response.items[0].snippet.title,
+        videoDesc: response.items[0].snippet.description,
+      };
+  },
+});
+
+// Videos collection - add API fetch here
+var VideosCollection = Backbone.Collection.extend({
+  model: VideoModel,
+
+  initialize: function () {
+    this.on('add', function (model) {
+      model.fetch();
+    })
   }
 });
 
-var videos = new VideosCollection();
-videos.on('add', function (i) { console.log(i.toJSON()); });
-videos.fetch();
+var AppModel = Backbone.Model.extend({
+  defaults: function () {
+    return {
+      videos: new VideosCollection(),
+    };
+  }
+});
+
+var VideoView = Backbone.View.extend({
+  // template: Handlebars.compile($('#main-display-template').html()),
+
+  // render: function () {
+  //   this.$el.html(this.template(this.model.toJSON()));
+  // }
+});
+
+var AppView = Backbone.View.extend({
+  el: $("body"),
+
+  events: {
+    "click .search": "newSearch", 
+  },
+  
+  initialize: function () {
+    this.$search = this.$('#search');
+    this.listenTo(this.model.get('videos'), 'change', this.renderVideo);
+    this.$mainspot = this.$('.main-spot');
+  },
+
+  newSearch: function () {
+    var topic = this.$search.val();
+
+    this.model.get('videos').add({
+      topic: topic
+    });
+  },
+
+  renderVideo: function (model) {
+    console.log(model);
+    var videoView = new VideoView({ model: model });
+
+    this.$mainspot.append(videoView.render().el);
+    debugger;
+  }
+});
+
+var appModel = new AppModel();
+var appView = new AppView ({ model: appModel });
+
