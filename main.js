ea0f0b4c50cde7db81b7c10c009e4220e3421264
Philip Krynicki <philipkrynicki@Philips-MacBook-Air.local>
2021-08-08 20:37:00 -0500

    Continued work on project.  Added five side thumbnails

--- main.js
+++ main.js
@@ -2,18 +2,54 @@
 //  Have a dedicated main player for the video, and five videos on the side
 //  When a user clicks a video in the list on the right, that clicked video becomes the main video
 //  When the above video changes, the title and description beneath the main video updates (through handlebars)
-//  When the page first loads, their is a default string of videos that loads.  The user can then change videos as desired.  Search bar empty when page loads
+//  When the page first loads, there is a default string of videos that loads.  The user can then change videos as desired.  Search bar empty when page loads
 
+$('.search-button').on('click', function () {
+  var video = $('#video-search').val();
+  
+
+  // $('#video-search').val('');
+  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${video}&type=video&videoEmbeddable=true&key=AIzaSyBG5b1D7dEBXFY0PgaKlMSFBcV3rNA7A8w`).then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+  renderMainVideo(data.items[0]);
+  renderSideVideos(data.items);
+  });
+  // // $('#video-search').val('');
+  // fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBG5b1D7dEBXFY0PgaKlMSFBcV3rNA7A8w&q=searchTerm`).then(function(response) {
+  //   return response.json();
+  // })
+  // .then(function(data) {
+  //   renderSideVideos(data);
+  //   console.log(data);
+  // });
+});
 
 
 
 var renderMainVideo = function (mainVideoInfo) {
   $('.mainVideo').empty();
-  console.log(mainVideoInfo);
+  // console.log(mainVideoInfo);
   var source = $('#mainvideo-template').html();
   var template = Handlebars.compile(source);
   var newHTML = template({mainVideoInfo});
   $('.mainVideo').append(newHTML);
+};
+
+var renderSideVideos = function (fiveVideoInfo) {
+  $('.sideVideo').empty();
+  // console.log(fiveVideoInfo);
+  var source = $('#fivevideo-template').html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < fiveVideoInfo.length; i++) {
+    var newHTML = template(
+      {
+        videoLoop: fiveVideoInfo[i]
+      }
+      );
+    $('.sideVideo').append(newHTML);
+  }
 };
 
 
@@ -43,46 +79,46 @@ var renderMainVideo = function (mainVideoInfo) {
 
 var apiData = {
   "kind": "youtube#searchListResponse",
-  "etag": "kFKJiEMd4H4PGTAgfZyDkMeOMK0",
-  "nextPageToken": "CAYQAA",
+  "etag": "f_RJ0O4oCnyC4jBijPkeDQWHJyI",
+  "nextPageToken": "CAUQAA",
   "regionCode": "US",
   "pageInfo": {
     "totalResults": 1000000,
-    "resultsPerPage": 6
+    "resultsPerPage": 5
   },
   "items": [
     {
       "kind": "youtube#searchResult",
-      "etag": "dx5MxJ267WsYyYihfeasbDJU98A",
+      "etag": "0Tob_RJFQzCx1XK9ZTCu6s6FnQQ",
       "id": {
         "kind": "youtube#video",
-        "videoId": "srlzTHIr3W0"
+        "videoId": "LJJ09p5S5-Y"
       },
       "snippet": {
-        "publishedAt": "2021-08-06T17:00:27Z",
-        "channelId": "UCHEf6T_gVq4tlW5i91ESiWg",
-        "title": "Try Not To Eat Challenge - Disney (Raya, Luca, Moana, Zootopia) | People Vs. Food",
-        "description": "We found Disney's tastiest foods to tempt our reactors! Can they resist these delicious foods?! Watch every episode of Try Not To Eat here: ...",
+        "publishedAt": "2021-08-08T21:51:20Z",
+        "channelId": "UCmOy2-vrvwDh6O1bTUB5m_g",
+        "title": "Yesterworld: The Rise &amp; Fall of Disney’s Pleasure Island and the Troubled History of Downtown Disney",
+        "description": "Explore the History of Pleasure Island, the Disney's first (and only) Adult oriented Entertainment District, as well as the troubled History & Origins of Downtown ...",
         "thumbnails": {
           "default": {
-            "url": "https://i.ytimg.com/vi/srlzTHIr3W0/default.jpg",
+            "url": "https://i.ytimg.com/vi/LJJ09p5S5-Y/default.jpg",
             "width": 120,
             "height": 90
           },
           "medium": {
-            "url": "https://i.ytimg.com/vi/srlzTHIr3W0/mqdefault.jpg",
+            "url": "https://i.ytimg.com/vi/LJJ09p5S5-Y/mqdefault.jpg",
             "width": 320,
             "height": 180
           },
           "high": {
-            "url": "https://i.ytimg.com/vi/srlzTHIr3W0/hqdefault.jpg",
+            "url": "https://i.ytimg.com/vi/LJJ09p5S5-Y/hqdefault.jpg",
             "width": 480,
             "height": 360
           }
         },
-        "channelTitle": "People Vs Food",
+        "channelTitle": "Yesterworld Entertainment",
         "liveBroadcastContent": "none",
-        "publishTime": "2021-08-06T17:00:27Z"
+        "publishTime": "2021-08-08T21:51:20Z"
       }
     },
     {
@@ -155,104 +191,70 @@ var apiData = {
     },
     {
       "kind": "youtube#searchResult",
-      "etag": "fECD5ZMlTPcUkTQ9yI4lOMPmi3s",
+      "etag": "qD04O7k7hFkSI9RzfLr1Aco7h2k",
       "id": {
         "kind": "youtube#video",
-        "videoId": "U5Vm_AiINuw"
+        "videoId": "qHo6-pjpCZY"
       },
       "snippet": {
-        "publishedAt": "2021-07-30T09:12:11Z",
-        "channelId": "UCFLHD_brc3GX49iZ9UOzrxQ",
-        "title": "[LIVE] Paper Dolls Dress Up 🔴 Disney&#39;s Marie Cat Handmade Dresses Quiet Book 🔴 Barbie Story Crafts",
-        "description": "woadollcrafts #paperdolls #paper #doll #woavideos #woadiy #woacrafts #paperdoll ----------- See more: - ALL VIDEOS WOA DOLLCRAFTS: ...",
+        "publishedAt": "2021-08-07T01:47:32Z",
+        "channelId": "UC3HHLghSCKX6pY3YE5PsyyQ",
+        "title": "Live: Epcot Food and Wine &amp; Epcot Forever Fireworks | Disney World",
+        "description": "epcotfoodandwine #live #epcotforever Check out the best Disney World You Tube website! livingindiz.com Welcome to Live: Epcot Food and Wine, Epic Live ...",
         "thumbnails": {
           "default": {
-            "url": "https://i.ytimg.com/vi/U5Vm_AiINuw/default_live.jpg",
+            "url": "https://i.ytimg.com/vi/qHo6-pjpCZY/default_live.jpg",
             "width": 120,
             "height": 90
           },
           "medium": {
-            "url": "https://i.ytimg.com/vi/U5Vm_AiINuw/mqdefault_live.jpg",
+            "url": "https://i.ytimg.com/vi/qHo6-pjpCZY/mqdefault_live.jpg",
             "width": 320,
             "height": 180
           },
           "high": {
-            "url": "https://i.ytimg.com/vi/U5Vm_AiINuw/hqdefault_live.jpg",
+            "url": "https://i.ytimg.com/vi/qHo6-pjpCZY/hqdefault_live.jpg",
             "width": 480,
             "height": 360
           }
         },
-        "channelTitle": "WOA Doll Crafts",
+        "channelTitle": "Living in DIZ",
         "liveBroadcastContent": "live",
-        "publishTime": "2021-07-30T09:12:11Z"
+        "publishTime": "2021-08-07T01:47:32Z"
       }
     },
     {
       "kind": "youtube#searchResult",
-      "etag": "R3iStJM4-FRFd8QCoGOn_Xq-bD0",
+      "etag": "SfEThKS1S0_hNEjpiWsPca3FVUs",
       "id": {
         "kind": "youtube#video",
-        "videoId": "togmdDHG3Pw"
+        "videoId": "XOnHtStmbCI"
       },
       "snippet": {
-        "publishedAt": "2021-07-08T12:43:38Z",
-        "channelId": "UC_976xMxPgzIa290Hqtk-9g",
-        "title": "Disney&#39;s Encanto | Teaser Trailer",
-        "description": "You're invited into the exceptional, fantastical and magical Casa Madrigal. ✨ Watch the new trailer for Disney's Encanto now! See the movie this November.",
+        "publishedAt": "2019-08-03T13:45:01Z",
+        "channelId": "UCNcdbMyA59zE-Vk668bKWOg",
+        "title": "Mickey and Donald Have a Farm 🚜 | Full Episode | Mickey Mouse Clubhouse | Disney Junior",
+        "description": "Mickey, Donald, Minnie, Goofy, and Daisy plan to rescue the missing animals from the Clubhouse Farm! Watch Mickey Mouse Clubhouse on Disney Junior and ...",
         "thumbnails": {
           "default": {
-            "url": "https://i.ytimg.com/vi/togmdDHG3Pw/default.jpg",
+            "url": "https://i.ytimg.com/vi/XOnHtStmbCI/default.jpg",
             "width": 120,
             "height": 90
           },
           "medium": {
-            "url": "https://i.ytimg.com/vi/togmdDHG3Pw/mqdefault.jpg",
+            "url": "https://i.ytimg.com/vi/XOnHtStmbCI/mqdefault.jpg",
             "width": 320,
             "height": 180
           },
           "high": {
-            "url": "https://i.ytimg.com/vi/togmdDHG3Pw/hqdefault.jpg",
+            "url": "https://i.ytimg.com/vi/XOnHtStmbCI/hqdefault.jpg",
             "width": 480,
             "height": 360
           }
         },
-        "channelTitle": "Walt Disney Animation Studios",
+        "channelTitle": "Disney Junior",
         "liveBroadcastContent": "none",
-        "publishTime": "2021-07-08T12:43:38Z"
-      }
-    },
-    {
-      "kind": "youtube#searchResult",
-      "etag": "SHoDBWIZkzDjDQccg36RXcPFsbE",
-      "id": {
-        "kind": "youtube#video",
-        "videoId": "uh4dTLJ9q9o"
-      },
-      "snippet": {
-        "publishedAt": "2015-09-16T07:00:30Z",
-        "channelId": "UCgwv23FVv3lqh567yagXfNg",
-        "title": "Disney Music - Lava (Official Lyric Video from &quot;Lava&quot;)",
-        "description": "Download the song “Lava” here: http://smarturl.it/dpls1 Google Play: http://smarturl.it/dplgps1 Amazon: http://smarturl.it/dplams1 Streaming: ...",
-        "thumbnails": {
-          "default": {
-            "url": "https://i.ytimg.com/vi/uh4dTLJ9q9o/default.jpg",
-            "width": 120,
-            "height": 90
-          },
-          "medium": {
-            "url": "https://i.ytimg.com/vi/uh4dTLJ9q9o/mqdefault.jpg",
-            "width": 320,
-            "height": 180
-          },
-          "high": {
-            "url": "https://i.ytimg.com/vi/uh4dTLJ9q9o/hqdefault.jpg",
-            "width": 480,
-            "height": 360
-          }
-        },
-        "channelTitle": "DisneyMusicVEVO",
-        "liveBroadcastContent": "none",
-        "publishTime": "2015-09-16T07:00:30Z"
+        "publishTime": "2019-08-03T13:45:01Z"
       }
     }
   ]
@@ -260,7 +262,7 @@ var apiData = {
 
 $( document ).ready(function() {
   renderMainVideo(apiData.items[0]);
-  // render sidevideos(apiData.items)
+  renderSideVideos(apiData.items);
 });
 
 
@@ -482,3 +484,222 @@ $( document ).ready(function() {
 //   }
 //   ]
 //   }`
+
+
+
+// {
+//   "kind": "youtube#searchListResponse",
+//   "etag": "kFKJiEMd4H4PGTAgfZyDkMeOMK0",
+//   "nextPageToken": "CAYQAA",
+//   "regionCode": "US",
+//   "pageInfo": {
+//     "totalResults": 1000000,
+//     "resultsPerPage": 6
+//   },
+//   "items": [
+//     {
+//       "kind": "youtube#searchResult",
+//       "etag": "dx5MxJ267WsYyYihfeasbDJU98A",
+//       "id": {
+//         "kind": "youtube#video",
+//         "videoId": "srlzTHIr3W0"
+//       },
+//       "snippet": {
+//         "publishedAt": "2021-08-06T17:00:27Z",
+//         "channelId": "UCHEf6T_gVq4tlW5i91ESiWg",
+//         "title": "Try Not To Eat Challenge - Disney (Raya, Luca, Moana, Zootopia) | People Vs. Food",
+//         "description": "We found Disney's tastiest foods to tempt our reactors! Can they resist these delicious foods?! Watch every episode of Try Not To Eat here: ...",
+//         "thumbnails": {
+//           "default": {
+//             "url": "https://i.ytimg.com/vi/srlzTHIr3W0/default.jpg",
+//             "width": 120,
+//             "height": 90
+//           },
+//           "medium": {
+//             "url": "https://i.ytimg.com/vi/srlzTHIr3W0/mqdefault.jpg",
+//             "width": 320,
+//             "height": 180
+//           },
+//           "high": {
+//             "url": "https://i.ytimg.com/vi/srlzTHIr3W0/hqdefault.jpg",
+//             "width": 480,
+//             "height": 360
+//           }
+//         },
+//         "channelTitle": "People Vs Food",
+//         "liveBroadcastContent": "none",
+//         "publishTime": "2021-08-06T17:00:27Z"
+//       }
+//     },
+//     {
+//       "kind": "youtube#searchResult",
+//       "etag": "HbF_0OK_9dBWW1rmqkNvD-tO1GM",
+//       "id": {
+//         "kind": "youtube#video",
+//         "videoId": "g8NVwN0_mks"
+//       },
+//       "snippet": {
+//         "publishedAt": "2019-01-10T12:32:26Z",
+//         "channelId": "UCulsxDuZ0gU8lzhpPAjUwUg",
+//         "title": "Disney RELAXING PIANO Collection -Sleep Music, Study Music, Calm Music (Piano Covered by kno)",
+//         "description": "Disney #Disneypiano #knopianomusic 00:00 Someday My Prince Will Come (From \"Snow White and the Seven Dwarfs\") 02:18 A Dream is a Wish Your Heart ...",
+//         "thumbnails": {
+//           "default": {
+//             "url": "https://i.ytimg.com/vi/g8NVwN0_mks/default.jpg",
+//             "width": 120,
+//             "height": 90
+//           },
+//           "medium": {
+//             "url": "https://i.ytimg.com/vi/g8NVwN0_mks/mqdefault.jpg",
+//             "width": 320,
+//             "height": 180
+//           },
+//           "high": {
+//             "url": "https://i.ytimg.com/vi/g8NVwN0_mks/hqdefault.jpg",
+//             "width": 480,
+//             "height": 360
+//           }
+//         },
+//         "channelTitle": "kno Piano Music",
+//         "liveBroadcastContent": "none",
+//         "publishTime": "2019-01-10T12:32:26Z"
+//       }
+//     },
+//     {
+//       "kind": "youtube#searchResult",
+//       "etag": "Flb8Z4i-_dCoE40cHxADfPSldYQ",
+//       "id": {
+//         "kind": "youtube#video",
+//         "videoId": "VCZ-7GQP1Ko"
+//       },
+//       "snippet": {
+//         "publishedAt": "2019-12-29T17:45:01Z",
+//         "channelId": "UCNcdbMyA59zE-Vk668bKWOg",
+//         "title": "Nutty Tales 30 Minute Compilation! | Chip &#39;N Dale&#39;s Nutty Tales | Disney Junior",
+//         "description": "Chip and Dale enter a kite competition, plan a firework show, run a lemonade stand, and more! Watch Chip N Dale Nutty Tales on Disney Junior and in the ...",
+//         "thumbnails": {
+//           "default": {
+//             "url": "https://i.ytimg.com/vi/VCZ-7GQP1Ko/default.jpg",
+//             "width": 120,
+//             "height": 90
+//           },
+//           "medium": {
+//             "url": "https://i.ytimg.com/vi/VCZ-7GQP1Ko/mqdefault.jpg",
+//             "width": 320,
+//             "height": 180
+//           },
+//           "high": {
+//             "url": "https://i.ytimg.com/vi/VCZ-7GQP1Ko/hqdefault.jpg",
+//             "width": 480,
+//             "height": 360
+//           }
+//         },
+//         "channelTitle": "Disney Junior",
+//         "liveBroadcastContent": "none",
+//         "publishTime": "2019-12-29T17:45:01Z"
+//       }
+//     },
+//     {
+//       "kind": "youtube#searchResult",
+//       "etag": "fECD5ZMlTPcUkTQ9yI4lOMPmi3s",
+//       "id": {
+//         "kind": "youtube#video",
+//         "videoId": "U5Vm_AiINuw"
+//       },
+//       "snippet": {
+//         "publishedAt": "2021-07-30T09:12:11Z",
+//         "channelId": "UCFLHD_brc3GX49iZ9UOzrxQ",
+//         "title": "[LIVE] Paper Dolls Dress Up 🔴 Disney&#39;s Marie Cat Handmade Dresses Quiet Book 🔴 Barbie Story Crafts",
+//         "description": "woadollcrafts #paperdolls #paper #doll #woavideos #woadiy #woacrafts #paperdoll ----------- See more: - ALL VIDEOS WOA DOLLCRAFTS: ...",
+//         "thumbnails": {
+//           "default": {
+//             "url": "https://i.ytimg.com/vi/U5Vm_AiINuw/default_live.jpg",
+//             "width": 120,
+//             "height": 90
+//           },
+//           "medium": {
+//             "url": "https://i.ytimg.com/vi/U5Vm_AiINuw/mqdefault_live.jpg",
+//             "width": 320,
+//             "height": 180
+//           },
+//           "high": {
+//             "url": "https://i.ytimg.com/vi/U5Vm_AiINuw/hqdefault_live.jpg",
+//             "width": 480,
+//             "height": 360
+//           }
+//         },
+//         "channelTitle": "WOA Doll Crafts",
+//         "liveBroadcastContent": "live",
+//         "publishTime": "2021-07-30T09:12:11Z"
+//       }
+//     },
+//     {
+//       "kind": "youtube#searchResult",
+//       "etag": "R3iStJM4-FRFd8QCoGOn_Xq-bD0",
+//       "id": {
+//         "kind": "youtube#video",
+//         "videoId": "togmdDHG3Pw"
+//       },
+//       "snippet": {
+//         "publishedAt": "2021-07-08T12:43:38Z",
+//         "channelId": "UC_976xMxPgzIa290Hqtk-9g",
+//         "title": "Disney&#39;s Encanto | Teaser Trailer",
+//         "description": "You're invited into the exceptional, fantastical and magical Casa Madrigal. ✨ Watch the new trailer for Disney's Encanto now! See the movie this November.",
+//         "thumbnails": {
+//           "default": {
+//             "url": "https://i.ytimg.com/vi/togmdDHG3Pw/default.jpg",
+//             "width": 120,
+//             "height": 90
+//           },
+//           "medium": {
+//             "url": "https://i.ytimg.com/vi/togmdDHG3Pw/mqdefault.jpg",
+//             "width": 320,
+//             "height": 180
+//           },
+//           "high": {
+//             "url": "https://i.ytimg.com/vi/togmdDHG3Pw/hqdefault.jpg",
+//             "width": 480,
+//             "height": 360
+//           }
+//         },
+//         "channelTitle": "Walt Disney Animation Studios",
+//         "liveBroadcastContent": "none",
+//         "publishTime": "2021-07-08T12:43:38Z"
+//       }
+//     },
+//     {
+//       "kind": "youtube#searchResult",
+//       "etag": "SHoDBWIZkzDjDQccg36RXcPFsbE",
+//       "id": {
+//         "kind": "youtube#video",
+//         "videoId": "uh4dTLJ9q9o"
+//       },
+//       "snippet": {
+//         "publishedAt": "2015-09-16T07:00:30Z",
+//         "channelId": "UCgwv23FVv3lqh567yagXfNg",
+//         "title": "Disney Music - Lava (Official Lyric Video from &quot;Lava&quot;)",
+//         "description": "Download the song “Lava” here: http://smarturl.it/dpls1 Google Play: http://smarturl.it/dplgps1 Amazon: http://smarturl.it/dplams1 Streaming: ...",
+//         "thumbnails": {
+//           "default": {
+//             "url": "https://i.ytimg.com/vi/uh4dTLJ9q9o/default.jpg",
+//             "width": 120,
+//             "height": 90
+//           },
+//           "medium": {
+//             "url": "https://i.ytimg.com/vi/uh4dTLJ9q9o/mqdefault.jpg",
+//             "width": 320,
+//             "height": 180
+//           },
+//           "high": {
+//             "url": "https://i.ytimg.com/vi/uh4dTLJ9q9o/hqdefault.jpg",
+//             "width": 480,
+//             "height": 360
+//           }
+//         },
+//         "channelTitle": "DisneyMusicVEVO",
+//         "liveBroadcastContent": "none",
+//         "publishTime": "2015-09-16T07:00:30Z"
+//       }
+//     }
+//   ]
+// }
