98d1cd5a06a711d28884a6abd2688620a9c9421f
Ryan Passer <rpasser93@gmail.com>
2021-08-08 22:23:03 -0400

    Set up togglers for clicked videos

--- views/AppView.js
+++ views/AppView.js
@@ -2,7 +2,8 @@ var AppView = Backbone.View.extend({
   el: $('body'),
 
   events: {
-    'click .btn': 'handleQuerySearch'
+    'click .btn': 'handleQuerySearch',
+    'click .small-vid': 'handleThumbnailClick'
   },
 
   initialize: function () {
@@ -16,6 +17,12 @@ var AppView = Backbone.View.extend({
     var query = this.$('input').val()
     this.model.get('videos').updateQuery(query);
     this.model.get('videos').fetch();
+  },
+
+  handleThumbnailClick: function (e) {
+    $('.big-video-col').find('.toggler').addClass('hide-this');
+    var grabId = $(e.currentTarget).attr('data-id');
+    $('#'+grabId).toggleClass('hide-this');
   },
 
   renderBigVideo: function (video) {
