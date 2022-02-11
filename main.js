79d4000eef5ca245598cf61a2c73826d4cadf061
Ian Holmes <iqholmes@gmail.com>
2022-02-11 17:33:59 -0500

    search working and side video images rendering

--- main.js
+++ main.js
@@ -6,17 +6,6 @@ var VideoModel = Backbone.Model.extend({
       vidId: '',
       thumbnailUrl: '',
     }
-  },
-
-  urlRoot: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=crossfit&type=video&videoEmbeddable=true&key=AIzaSyAox3HZhZPjJitNh1kl5MUHB-BOPAnfNos`,
-
-  parse: function(response) {
-    return {
-      title: response.items[0].snippet.title,
-      description: response.items[0].snippet.description,
-      vidId: response.items[0].id.videoId,
-      thumbnailUrl: response.items[0].snippet.thumbnails.default.url,
-    }
   }
 })
 
@@ -33,23 +22,52 @@ var MainVideoView = Backbone.View.extend({
 })
 
 var SideVideoView = Backbone.View.extend({
-  
+  className: 'sideVideo',
+
+  template: Handlebars.compile($('#sideVideos-template').html()),
+
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()))
+
+    return this;
+  }
 })
 
-var VideoCollection = Backbone.Collection.extend({ 
+var VideoCollection = Backbone.Collection.extend({
+  url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=crossfit&type=video&videoEmbeddable=true&key=AIzaSyAox3HZhZPjJitNh1kl5MUHB-BOPAnfNos`,
+
   model: VideoModel,
+
+  parse: function(response) {
+    console.log(response)
+    return response.items.map(function(video){
+      return {
+        title: video.snippet.title,
+        description: video.snippet.description,
+        vidId: video.id.videoId,
+        thumbnailUrl: video.snippet.thumbnails.default.url,
+      }
+    })
+  },
 
   initialize: function() {
     this.on('add', function(model) {
       model.fetch();
     })
+  },
+
+  searchVideos: function(query) {
+    this.url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${query}&type=video&videoEmbeddable=true&key=AIzaSyAox3HZhZPjJitNh1kl5MUHB-BOPAnfNos`
+
+    this.fetch({ reset: true })
   }
 })
 
 var AppModel = Backbone.Model.extend({
   defaults: function() {
     return {
-      videos: new VideoCollection()
+      videos: new VideoCollection(),
+      mainVideo: null
     }
   } 
 })
@@ -63,212 +81,39 @@ var AppView = Backbone.View.extend({
 
   initialize: function() {
     this.$searchInput = this.$('#search-query');
-    this.$mainVideo = this.$('.mainVideo')
+    this.$mainVideo = this.$('.mainVideo');
+    this.$sideVideos = this.$('.sideVideos')
 
-    this.listenTo(this.model.get('videos'), 'change', this.renderMainVideo);
-    // this.listenTo(this.model.get('videos'), 'change', this.renderPlaylist)
+    this.listenTo(this.model.get('videos'), 'reset', function() {
+      this.renderMainVideo();
+      this.renderSideVideos();
+    });
   },
 
   handleSearchClick:function(){
     var searchTerm = this.$searchInput.val();
 
-    this.model.get('videos').add({
-      query: searchTerm
+    this.model.get('videos').searchVideos(searchTerm);
+  },
+
+  renderMainVideo: function() {
+    this.$mainVideo.html('');
+    var firstVideoModel = this.model.get('videos').at(0);
+
+    var videoView = new MainVideoView({model: firstVideoModel});
+
+    this.$mainVideo.append(videoView.render().el);
+  },
+
+  renderSideVideos: function() {
+    this.$sideVideos.html('');
+    this.model.get('videos').forEach(function (video) {
+      var sideVideoView = new SideVideoView({model: video});
+      this.$('.sideVideosList').append(sideVideoView.render().el);
     })
-  },
-
-  renderMainVideo: function(video) {
-    var mainVideoView = new MainVideoView({ model: video })
-    this.$mainVideo.append(mainVideoView.render().el)
-  },
-
-  renderPlaylist: function() {
-
   }
 });
 
 var appModel = new AppModel();
-var appView = new AppView({ model:appModel })
-
-var sampleData = {
-  "kind": "youtube#searchListResponse",
-  "etag": "jrLgj0XGHegO7nztkfHkqGu_dmo",
-  "nextPageToken": "CAUQAA",
-  "regionCode": "US",
-  "pageInfo": {
-    "totalResults": 1000000,
-    "resultsPerPage": 5
-  },
-  "items": [
-    {
-      "kind": "youtube#searchResult",
-      "etag": "BJ_pZJtlXOdlNB7-r4tAl81I3C4",
-      "id": {
-        "kind": "youtube#video",
-        "videoId": "L5dM6NdZXd4"
-      },
-      "snippet": {
-        "publishedAt": "2021-01-17T16:00:09Z",
-        "channelId": "UCBvnS6nyNGAl8EUNt-40xoQ",
-        "title": "Josh Bridges 2021 CrossFit Open Prep + Dave Castro Gets Called Out | Paying the Man Ep.083",
-        "description": "The CrossFit season is just around the corner so I show you how I am preparing for the 2021 CrossFit Open. I also call out Dave Castro, to get him and I in a ring, ...",
-        "thumbnails": {
-          "default": {
-            "url": "https://i.ytimg.com/vi/L5dM6NdZXd4/default.jpg",
-            "width": 120,
-            "height": 90
-          },
-          "medium": {
-            "url": "https://i.ytimg.com/vi/L5dM6NdZXd4/mqdefault.jpg",
-            "width": 320,
-            "height": 180
-          },
-          "high": {
-            "url": "https://i.ytimg.com/vi/L5dM6NdZXd4/hqdefault.jpg",
-            "width": 480,
-            "height": 360
-          }
-        },
-        "channelTitle": "Josh Bridges",
-        "liveBroadcastContent": "none",
-        "publishTime": "2021-01-17T16:00:09Z"
-      }
-    },
-    {
-      "kind": "youtube#searchResult",
-      "etag": "JFhM06PZ2Ck-Tu0ZALY18QhBKog",
-      "id": {
-        "kind": "youtube#video",
-        "videoId": "vcDsh9Sjm18"
-      },
-      "snippet": {
-        "publishedAt": "2020-10-23T16:43:38Z",
-        "channelId": "UCRs1pHnES3QDdh43xbjOmzw",
-        "title": "Event 1 &amp; 2 - 2007 Reload and Corn Sack Sprint - 2020 CrossFit Games",
-        "description": "The hardest test in CrossFit Games history begins here. Five men. Five women. Three days of grueling competition. Grab snacks. #CrossFitGames The CrossFit ...",
-        "thumbnails": {
-          "default": {
-            "url": "https://i.ytimg.com/vi/vcDsh9Sjm18/default.jpg",
-            "width": 120,
-            "height": 90
-          },
-          "medium": {
-            "url": "https://i.ytimg.com/vi/vcDsh9Sjm18/mqdefault.jpg",
-            "width": 320,
-            "height": 180
-          },
-          "high": {
-            "url": "https://i.ytimg.com/vi/vcDsh9Sjm18/hqdefault.jpg",
-            "width": 480,
-            "height": 360
-          }
-        },
-        "channelTitle": "CrossFit Games",
-        "liveBroadcastContent": "none",
-        "publishTime": "2020-10-23T16:43:38Z"
-      }
-    },
-    {
-      "kind": "youtube#searchResult",
-      "etag": "gW7BE77piDCVb3uIFDXHGyK1BNA",
-      "id": {
-        "kind": "youtube#video",
-        "videoId": "tzD9BkXGJ1M"
-      },
-      "snippet": {
-        "publishedAt": "2010-07-23T15:09:19Z",
-        "channelId": "UCtcQ6TPwXAYgZ1Mcl3M1vng",
-        "title": "What is CrossFit?",
-        "description": "What is CrossFit? CrossFit is an effective way to get fit. Anyone can do it. It is a fitness program that combines a wide variety of functional movements into a timed ...",
-        "thumbnails": {
-          "default": {
-            "url": "https://i.ytimg.com/vi/tzD9BkXGJ1M/default.jpg",
-            "width": 120,
-            "height": 90
-          },
-          "medium": {
-            "url": "https://i.ytimg.com/vi/tzD9BkXGJ1M/mqdefault.jpg",
-            "width": 320,
-            "height": 180
-          },
-          "high": {
-            "url": "https://i.ytimg.com/vi/tzD9BkXGJ1M/hqdefault.jpg",
-            "width": 480,
-            "height": 360
-          }
-        },
-        "channelTitle": "CrossFit®",
-        "liveBroadcastContent": "none",
-        "publishTime": "2010-07-23T15:09:19Z"
-      }
-    },
-    {
-      "kind": "youtube#searchResult",
-      "etag": "8KgGsXPicxGt8j-2-OuRhutT9s4",
-      "id": {
-        "kind": "youtube#video",
-        "videoId": "860T3WYvyoc"
-      },
-      "snippet": {
-        "publishedAt": "2020-10-23T20:16:34Z",
-        "channelId": "UCRs1pHnES3QDdh43xbjOmzw",
-        "title": "Event 3 - CrossFit Total - 2020 CrossFit Games",
-        "description": "The wait is finally over. Watch the three-day finale of the 2020 CrossFit Games right here. Buckle up for an unforgettable weekend. Every event of the final stage ...",
-        "thumbnails": {
-          "default": {
-            "url": "https://i.ytimg.com/vi/860T3WYvyoc/default.jpg",
-            "width": 120,
-            "height": 90
-          },
-          "medium": {
-            "url": "https://i.ytimg.com/vi/860T3WYvyoc/mqdefault.jpg",
-            "width": 320,
-            "height": 180
-          },
-          "high": {
-            "url": "https://i.ytimg.com/vi/860T3WYvyoc/hqdefault.jpg",
-            "width": 480,
-            "height": 360
-          }
-        },
-        "channelTitle": "CrossFit Games",
-        "liveBroadcastContent": "none",
-        "publishTime": "2020-10-23T20:16:34Z"
-      }
-    },
-    {
-      "kind": "youtube#searchResult",
-      "etag": "a53xv9Yh4o643yVpPbDCpFyCZIM",
-      "id": {
-        "kind": "youtube#video",
-        "videoId": "JZpVUNiSg5s"
-      },
-      "snippet": {
-        "publishedAt": "2020-12-28T18:09:41Z",
-        "channelId": "UCEQi1ZNJiw3YMRwni0OLsTQ",
-        "title": "I TRIED CROSSFIT FOR THE FIRST TIME",
-        "description": "Trying a crossfit workout for the first time with the @Buttery Bros BUTTERY BROS https://www.youtube.com/channel/UCp00ppkfPFBUTiiNNd8kS9Q BUTTERY ...",
-        "thumbnails": {
-          "default": {
-            "url": "https://i.ytimg.com/vi/JZpVUNiSg5s/default.jpg",
-            "width": 120,
-            "height": 90
-          },
-          "medium": {
-            "url": "https://i.ytimg.com/vi/JZpVUNiSg5s/mqdefault.jpg",
-            "width": 320,
-            "height": 180
-          },
-          "high": {
-            "url": "https://i.ytimg.com/vi/JZpVUNiSg5s/hqdefault.jpg",
-            "width": 480,
-            "height": 360
-          }
-        },
-        "channelTitle": "Whitney Simmons",
-        "liveBroadcastContent": "none",
-        "publishTime": "2020-12-28T18:09:41Z"
-      }
-    }
-  ]
-}
+var appView = new AppView({ model:appModel });
+appModel.get('videos').fetch({ reset: true });
