4d8c87f8877e8077cbf70159cd890b002fbedcb8
Jeremy Nielsen <jeremy.nielsen83@gmail.com>
2022-03-13 17:43:09 -0400

    streamlined?

--- views/VideoListView.js
+++
@@ -1,8 +0,0 @@
-var VideoListView = Backbone.View.extend({
-  template: Handlebars.compile($('#list-video-template').html()),
-
-  render: function () {
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;
-  }
-}) 
--- views/VideoView.js
+++ views/VideoView.js
@@ -6,3 +6,12 @@ var VideoView = Backbone.View.extend({
     return this;
   }
 }) 
+
+var VideoListView = Backbone.View.extend({
+  template: Handlebars.compile($('#list-video-template').html()),
+
+  render: function () {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+}) 
