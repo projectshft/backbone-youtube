0490d2a486356cf027342537ced78d1f70e0a6c5
Doug <douglasharrisonray@gmail.com>
2022-03-17 23:25:37 -0600

    obj of array of results

--- models/VideoModel.js
+++ models/VideoModel.js
@@ -1,13 +1,31 @@
 var VideoModel = Backbone.Model.extend({
 defaults: {
-  id: '',
-  // thumbnail: '',
-  // title: '',
-  // description: ''
+  videoId: '',
+  thumbnail: '',
+  title: '',
+  description: ''
 },
 
 urlRoot: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&type=video&videoEmbeddable=true&key=AIzaSyADSKbyZhQ9S8rj3F3G_3Ihz0L2BRcqQLQ&q=",
 
-
+// parse: function (response) {
+//   var array = response.items
+//   return {
+//     id: response.etag,
+//     // thumbnail: response.items.snippet.thumbnails.default.url,
+//     // title: response.items.snippet.title,
+//     // description: response.items.snippet.description
+//   },
+parse: function (response) {
+  // var array = 
+  return response.items.map(function (issues) {
+    return {
+      videoId: issues.id.videoId,
+      thumbnail: issues.snippet.thumbnails.default.url,
+      title: issues.snippet.title,
+      description: issues.snippet.description
+    }
+  });
+}
 
 });
---
+++ views/VideoView.js
@@ -0,0 +1,16 @@
+var ViewView = Backbone.View.extend({
+  className: 'video',
+
+  template: Handlebars.compile($('.main-video').html()),
+
+  // initialize: function () {
+  //   this.listenTo(this.model)
+  // }
+
+  render: function () {
+    this.$el.html(this.template(this.model.toJSON()));
+
+    return this;
+  }
+
+});
