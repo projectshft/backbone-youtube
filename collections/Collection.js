9008bf277e55aab06412df47e25eff1d643af6d3
Jeremynielsen <95871403+Jeremynielsen@users.noreply.github.com>
2022-03-19 13:08:24 -0400

    Add files via upload

---
+++ collections/Collection.js
@@ -0,0 +1,21 @@
+var VideoCollection = Backbone.Collection.extend({
+  url: '',
+
+  model: VideoModel,
+
+  setUrl: function (query) {
+    this.url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${query}&type=video&videoEmbeddable=true&key=AIzaSyBKne27xN5AjVzSaQLMS9d3nNz469DDVDA`;
+  },
+
+
+  parse: function (response) {
+    return response.items.map(function (video) {
+      return {
+        id: video.id.videoId,
+        title: video.snippet.title,
+        description: video.snippet.description,
+        thumb: video.snippet.thumbnails.default.url
+      }
+    }); 
+  }
+}) 
---
+++ models/AppModel.js
@@ -0,0 +1,14 @@
+var AppModel = Backbone.Model.extend({
+  defaults: function () {
+    return {
+      videos: new VideoCollection(),
+      currentVideo: null
+    }
+  },
+
+  setCurrentVideo: function (id) {
+    var newVideo = this.get('videos').findWhere({id: id});
+    this.set('currentVideo', newVideo);
+  }
+}) 
+//staging comment
