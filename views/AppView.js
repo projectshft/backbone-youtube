e389ab7d56cb74366d9145da461c619fd716ffa4
Yegor Rodin <rodin4doc@gmail.com>
2022-03-28 10:47:16 -0400

    edit userSearch func

--- views/AppView.js
+++ views/AppView.js
@@ -12,9 +12,7 @@ var AppView = Backbone.View.extend({
     console.log('test');
     if(this.$('.input').val()) {
       var userInput = this.$('.input').val();   
-      var url =`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${userInput}&type=video&videoEmbeddable=true&key=AIzaSyCuZkTkLT7pAbGYodRnTSHgzDYtty53nwU`;
-      var appModel = new AppModel({videos: new VideoCollection({url: url})});
-      var videoCollection = new VideoCollection();
+     
       console.log(videoCollection);
     }
   },
