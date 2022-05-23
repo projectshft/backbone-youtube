78879d9c939a2a1b4350ce48f40bb52d990e2d23
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-05-23 11:39:59 -0400

    connected and tested Handlebars

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const source = $('#app-view').html();
+// turn our "template" into html
+var source = $('#store-template').html();
 
-const template = Handlebars.compile(source);
+// compile our template html using handlebars
+var template = Handlebars.compile(source);
 
-const testHTML = template({test: 'testing'});
+// fill our template with information
+var newHTML = template({item: "bread", price: "3"});
 
-$('.test').append(testHTML);
+// append our new html to the page
+$('.items').append(newHTML);
 
+var newHTML2 = template({item: "Cheese", price: "10"});
+
+$('.items').append(newHTML2);
