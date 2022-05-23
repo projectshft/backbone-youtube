5d27045c8042a784da57b437f6b13bfc75a5b389
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-05-23 13:36:51 -0400

    narrowed down handlebars test code for future use

--- main.js
+++ main.js
@@ -14,19 +14,3 @@ var newHTML2 = template({item: "Cheese", price: "10"});
 
 $('.items').append(newHTML2);
 
-//2nd test
-var source2 = $('#store-template-2').html();
-
-// compile our template html using handlebars
-var template2 = Handlebars.compile(source2);
-
-// fill our template with information
-var newHTML3 = template2({pizza: 'sausage'});
-
-console.log(`new HTML 1 ${newHTML}`);
-console.log(`new HTML 2 ${newHTML2}`);
-console.log(`new HTML 3 ${newHTML3}`);
-
-// append our new html to the page
-$('.test-2').append(newHTML3);
-
