0d04ae30d566766841309d9ec0c21c093b3f96e8
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-05-24 08:59:00 -0400

    I have videoViewer and videoModel connected ad displaying defaults when need be

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
-const VideoViewer = Backbone.Model.extend({
-  defaults: function () {
-    return {
-      video: '',
-      title: '',
-      description: ''
-    }
+const VideoViewerModel = Backbone.Model.extend({
+  defaults: {
+    video: '',
+    title: '',
+    description: ''
   }
 })
 
 
+const VideoViewerViewer = Backbone.View.extend({
+  template: Handlebars.compile($('#video-view-template').html())
+
+});
+
+const videoModel = new VideoViewerModel;
+
+const videoViewer = new VideoViewerViewer({ model: videoModel });
+
+
+
+
+//-----------------------
+//-----------------------vi
+//-----------------------
 //HANDLEBARS SOURCE CODE
+//-----------------------
+//-----------------------
+//-----------------------
 // turn our "template" into html
-var source = $('#video-view-template').html();
+// var source = $('#video-view-template').html();
 
-// compile our template html using handlebars
-var template = Handlebars.compile(source);
+// // compile our template html using handlebars
+// var template = Handlebars.compile(source);
 
-// fill our template with information
-var newHTML = template({item: "bread", price: "3"});
+// // fill our template with information
+// var newHTML = template({item: "bread", price: "3"});
 
-// append our new html to the page
-$('.items').append(newHTML);
+// // append our new html to the page
+// $('.items').append(newHTML);
 
-var newHTML2 = template({item: "Cheese", price: "10"});
+// var newHTML2 = template({item: "Cheese", price: "10"});
 
-$('.items').append(newHTML2);
+// $('.items').append(newHTML2);
 
