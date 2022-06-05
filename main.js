55056b78ebba161169a436148682e04135db12df
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-06-05 15:31:49 -0400

    paired down

--- main.js
+++ main.js
@@ -1,5 +1,2 @@
 const videoCollection = new VideoCollection();
 
-const videoModel = new VideoModel({img: 'face', title: 'face vid', description: 'This is a video of a face!'});
-
-videoCollection.add(videoModel)
