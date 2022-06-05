5d85f9f259673605012d9f5a3642baa51f43f619
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-06-05 16:38:58 -0400

    description successfully posts too

--- Views/AppView.js
+++ Views/AppView.js
@@ -10,12 +10,15 @@ const AppView = Backbone.View.extend({
     // console.log(searchBarVal);
     const newImg = sampleData.items[0].snippet.thumbnails.high.url
     const newTitle = sampleData.items[0].snippet.title;
-    const newVideoModel = new VideoModel({img: newImg, title: newTitle});
+    const newDescription = sampleData.items[0].snippet.description;
+    const newVideoModel = new VideoModel({img: newImg, title: newTitle, description: newDescription});
     videoCollection.add(newVideoModel);
     const img = videoCollection.models[0].attributes.img;
     const title = videoCollection.models[0].attributes.title;
+    const description = videoCollection.models[0].attributes.description;
     $('.main-video-row').append(`<img src=${img}>`);
     $('.main-video-title-row').append(`<h3>${title}</h3>`)
+    $('.main-video-description-row').append(`<p>${description}</p>`)
   }
 
 
