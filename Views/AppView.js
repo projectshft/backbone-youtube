c133ce04a7e792ead5006b5b87ac780f83f13c87
Joshua Clarke Cushing <joshuacushing@new-host-9.home>
2022-06-06 12:18:23 -0400

    submitMainVideoHandler does not append twice

--- Views/AppView.js
+++ Views/AppView.js
@@ -16,17 +16,22 @@ const AppView = Backbone.View.extend({
   },
 
   submitMainVideoHandler: function() {
-    const newImg = sampleData.items[0].snippet.thumbnails.high.url
-    const newTitle = sampleData.items[0].snippet.title;
-    const newDescription = sampleData.items[0].snippet.description;
-    const newVideoModel = new VideoModel({img: newImg, title: newTitle, description: newDescription});
-    videoCollection.add(newVideoModel);
-    const img = videoCollection.models[0].attributes.img;
-    const title = videoCollection.models[0].attributes.title;
-    const description = videoCollection.models[0].attributes.description;
-    $('.main-video-row').append(`<img src=${img}>`);
-    $('.main-video-title-row').append(`<h3 id="main-video-title">${title}</h3>`)
-    $('.main-video-description-row').append(`<p id="main-video-description">${description}</p>`)
+
+    if (videoCollection.length === 0) {
+      const newImg = sampleData.items[0].snippet.thumbnails.high.url
+      const newTitle = sampleData.items[0].snippet.title;
+      const newDescription = sampleData.items[0].snippet.description;
+      const newVideoModel = new VideoModel({img: newImg, title: newTitle, description: newDescription});
+      videoCollection.add(newVideoModel);
+      const img = videoCollection.models[0].attributes.img;
+      const title = videoCollection.models[0].attributes.title;
+      const description = videoCollection.models[0].attributes.description;
+      $('.main-video-row').append(`<img src=${img}>`);
+      $('.main-video-title-row').append(`<h3 id="main-video-title">${title}</h3>`)
+      $('.main-video-description-row').append(`<p id="main-video-description">${description}</p>`)
+    }
+
+    
   },
 
   submitSideVideoHandler: function(index, idNum) {
