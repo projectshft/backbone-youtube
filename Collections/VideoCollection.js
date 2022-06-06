9db882f895aaf14c9a93ca46d2d66b04a7bc0c9d
Joshua Clarke Cushing <joshuacushing@new-host-9.home>
2022-06-06 14:35:43 -0400

    base url set for collection

--- Collections/VideoCollection.js
+++ Collections/VideoCollection.js
@@ -1,3 +1,9 @@
+const APIKey = 'AIzaSyAwCSZgkgbJ98nIxRAbxjIROi7QzKlrCvc';
+
+const baseURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=chess&type=video&videoEmbeddable=true&key='
+
 const VideoCollection = Backbone.Collection.extend({
+  url: baseURL + APIKey,
+
   model: VideoModel
 })
