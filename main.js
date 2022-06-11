d879e7716e278297f08da76ff82e7d8cc994af10
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-06-11 13:39:20 -0400

    found that fetch was adding the fetched data to the collection

--- main.js
+++ main.js
@@ -7,5 +7,5 @@ const videoModel = new VideoModel();
 const mainVideoView = new MainVideoView();
 
 appModel.get('videos').fetch({reset: true})
-.done(function(res) {console.log(res)})
+.done(function(res) {console.log('fetch was successful')})
 .fail(function() {console.log('fetch failed')})
