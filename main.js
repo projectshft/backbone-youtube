4441832b331bf172806fe238287b0b903c073734
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-06-11 16:06:44 -0400

    cleaned up main.js

--- main.js
+++ main.js
@@ -5,7 +5,3 @@ const appView = new AppView({model: appModel});
 const videoModel = new VideoModel();
 
 const mainVideoView = new MainVideoView();
-
-// appModel.get('videos').fetch({reset: true})
-// .done(function(res) {console.log(res)})
-// .fail(function() {console.log('fetch failed')})
