b29ad7286ea6e0994fa8c8a2c403c3e6a11dacab
drkstn <86159096+drkstn@users.noreply.github.com>
2022-06-11 05:57:34 -0700

    add selected video render

--- main.js
+++ main.js
@@ -22,10 +22,21 @@ var VideoCollection = Backbone.Collection.extend({
   }
 });
 
-var VideoView = Backbone.View.extend({
+var VideosView = Backbone.View.extend({
   className: 'v-search-result',
   
   template: Handlebars.compile($('#v-search-result-template').html()),
+
+  render: function () {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+});
+
+var VideoSelectedView = Backbone.View.extend({
+  className: 'v-selected',
+  
+  template: Handlebars.compile($('#v-selected-template').html()),
 
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
@@ -41,6 +52,16 @@ var AppModel = Backbone.Model.extend({
       videos: new VideoCollection(),
       selected_video: null
     }
+  },
+
+  setSelectedVideo: function () {
+    this.set('selected_video', this.get('videos').at(0));
+  },
+
+  updateSelectedVideo: function (id) {
+    var selectedVideo = this.get('videos').findWhere({ videoId: id });
+
+    this.set('selected_video', selectedVideo);
   }
 });
 
@@ -48,27 +69,59 @@ var AppView = Backbone.View.extend({
   el: $('.container'),
   
   events: {
-    'click .search': 'handleSearchClick'
+    'click .search' : 'handleSearchClick',
+    'click .thumbnail' : 'selectVideo'
   },
 
   initialize: function () {
-    this.$searchInput = this.$('#search-query');
-
     this.listenTo(this.model.get('videos'), 'add', this.renderVideo);
-    this.renderVideos();
+    this.listenTo(this.model.get('videos'), 'reset', this.renderVideos);
+    this.listenTo(this.model, 'change:selected_video', this.renderSelectedVideo);
+    // this.renderSelectedVideo();
+    // this.renderVideos();
   },
 
   handleSearchClick: function () {
-    var searchQuery = this.$searchInput.val();
-    console.log(searchQuery)
+    var searchQuery = this.$('#search-query').val();
+    console.log(searchQuery);
+    this.model.get('videos').reset();
+    
+    sampleData.items.forEach((item, index) => {
+      console.log('Video ' + index + ' loaded');
+      appModel.get('videos').addVideo(
+        item.id.videoId,
+        item.snippet.title,
+        item.snippet.description,
+        item.snippet.thumbnails.default.url
+      )
+    });
+
+    this.model.setSelectedVideo();
+  },
+
+  selectVideo: function (e) {
+    var selectedVideoId = $(e.currentTarget).data().video_id;
+
+    this.model.updateSelectedVideo(selectedVideoId);
+  },
+
+  createSelectedVideo: function (video) {
+    var videoSelectedView = new VideoSelectedView({ model: video });
+    this.$('.v-selected-container').empty();
+    this.$('.v-selected-container').append(videoSelectedView.render().el);
+  },
+
+  renderSelectedVideo: function () {
+    this.createSelectedVideo(this.model.get('selected_video'));
   },
 
   renderVideo: function (video) {
-    var videoView = new VideoView({ model: video });
-    this.$('.v-list').append(videoView.render().el)
+    var videosView = new VideosView({ model: video });
+    this.$('.v-list-container').append(videosView.render().el);
   },
 
   renderVideos: function () {
+    this.$('.v-list-container').empty();
     this.model.get('videos').each(function (video) {
       this.renderVideo(video);
     }, this);
@@ -78,16 +131,5 @@ var AppView = Backbone.View.extend({
 //////////////////////////////////////////////////////
 
 var appModel = new AppModel();
-
-// Sample starter data
-sampleData.items.forEach((item, index) => {
-  console.log('Video ' + index + ' loaded');
-  appModel.get('videos').addVideo(
-    item.id.videoId,
-    item.snippet.title,
-    item.snippet.description,
-    item.snippet.thumbnails.default.url
-  )
-});
 
 var appView = new AppView({ model: appModel });
