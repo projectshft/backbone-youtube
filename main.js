9ceeef22074912ac54ddd0d438eb7022c6f85712
Braydon Sutherland <geomatics@Braydons-MacBook-Air.local>
2022-06-15 14:51:21 -0500

    setup

---
+++ main.js
@@ -0,0 +1,3 @@
+// Youtube API Key:  AIzaSyBJ_kn8KJdIxc928Ml9RAwvDlThYNy6vDw
+// HTTP Request URL: https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q={query}&type=video&videoEmbeddable=true&key={apiKey}//
+// Embed <iframe width="560" height="315" src="https://www.youtube.com/embed/{videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> //
