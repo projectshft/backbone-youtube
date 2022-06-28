ccf06d8ee474069244aef993dbf2746634b04cf9
Jill Arnold <jillarnold2913@gmail.com>
2022-06-28 11:41:19 -0400

    First attempt at reworking; click is successful, trying to explore how to link searchName to "q="

--- main.js
+++ main.js
@@ -1,39 +1,48 @@
 var AppModel = Backbone.Model.extend({
   defaults: function () {
     return {
-     videos,
+      searchName: "funniest dog videos"
     }
-  }
-});
+    }
+  });
 
-var appModel = new AppModel()
-
-var VideosCollection = Backbone.Collection.extend({
-  model: AppModel,
-  url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=dogs&type=video&videoEmbeddable=true&key=AIzaSyDkiczzrnArBHYPLDSsfeCDbKVsJxALcZc",
-});
 
 var AppView = Backbone.View.extend({
-  template: Handlebars.compile($('#side-videos-template').html()),
-  
-  events: {    
-    'click .btn': 'renderVideos'
+  el: $('.appView-container'),
+
+  events: {
+    'click .btn': 'initiateSearch',
   },
 
-renderVideo: function () {
-  //console.log('click');
-  this.model.get('videos').each(function (AppModel) {
-    var currentVideoView = new AppView({ model: AppModel });
-      this.$('.video-container').append(currentVideoView.render().el);
-}, this);
- },
+ 
+
+  initialize: function () {
+    console.log('click')
+    this.listenTo(this.model, 'change:searchName', this.renderVideo)
+    
+  },
+
+  initiateSearch: function () {
+    var searchName = this.$el.find('#search-query').val();
+
+    this.model.set('searchName', searchName);
+    
+  },
+
+
+  renderText: function () {
+    this.$el.find('searchName').html(this.template(this.model.toJSON()))
+  }
+
+
 });
 
-var appView = new AppView();
-   
-var videos = new VideosCollection();
-  videos.fetch({
-   success: function () {
-     console.log(videos.models);
-   }
-  });
+
+
+  var appModel = new AppModel();
+  var appView = new AppView({ model: appModel });
+
+
+ 
+
+
